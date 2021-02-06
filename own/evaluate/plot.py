6b5cc663b1958376953c071bb2f2f99f7f6fcf0e
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-02-06 15:55:41 +0100

    First version of evaluating via plotting. Matplotlib doesnt support pdfs that large so I have to use a different engine. However, this is a good example how subplotting works, so I will commit it :;D

--- own/evaluate/plot.py
+++ own/evaluate/plot.py
@@ -4,32 +4,92 @@ import pickle as pkl
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+price_col = "Close"
+colors = {"hold": "y", "buy": "g", "sell": "r"}
+
 with open(paths.data_path / "evaluated.pkl", "rb") as f:
     data = pkl.load(f)
 
-price_col = "Close"
 
-data = data[0]
-df = data["data"]
-df = df.drop(df.tail(1).index)
-df = df[[price_col, "actions"]]
-df = df.replace({0: "hold", 1: "buy", 2: "sell"})
+def prepare(grp):
+    df = grp["data"]
+    df = df.drop(df.tail(1).index)
+    df = df[[price_col, "actions"]]
+    df["actions"] = df["actions"].replace({0: "hold", 1: "buy", 2: "sell"})
+    return df
 
-prices = df[price_col].values.tolist()
-actions = df["actions"].values.tolist()
 
-colors = {"hold": "y", "buy": "g", "sell": "r"}
-x = []
-y = []
-hue = []
-for i, (price, action) in enumerate(zip(prices, actions)):
-    x.append(i)
-    y.append(price)
-    hue.append(action)
+def generate_points(prices, actions):
+    x = []
+    y = []
+    hue = []
+    for i, (price, action) in enumerate(zip(prices, actions)):
+        x.append(i)
+        y.append(price)
+        hue.append(action)
+    return x, y, hue
 
-sns.pointplot(x=x, y=y, hue=hue, palette=colors, linestyles="")
 
-sns.lineplot(data=df[price_col], color="black")
+def calculate_profit(prices, actions):
+    # In case we didn't buy anything there is also not profit/loss
+    if "buy" not in actions:
+        return "-"
 
-plt.title(f"Ticker: {data['ticker']}, Profit: {round(data['total_profit'], 2)}")
-plt.show()
+    profit = 1
+    inventory = []
+
+    for price, action in zip(prices, actions):
+        if action == "hold":
+            continue
+        elif action == "buy":
+            inventory.append(price)
+        elif action == "sell":
+            if inventory:
+                for inv in inventory:
+
+                    if inv == 0.0:
+                        inv += 0.01
+
+                    profit *= price / inv
+                inventory = []
+            else:
+                continue
+
+    return round(profit, 2)
+
+
+max_stop = 50
+
+if max_stop == -1:
+    max_stop = len(data)
+
+plt.figure(1, dpi=300, figsize=(15, max_stop * 10))
+
+for stop, grp in enumerate(data):
+
+    print(f"Processing {stop}/{max_stop}")
+
+    if stop == max_stop:
+        break
+
+    plt.subplot(max_stop, 1, 1 + stop)
+
+    df = prepare(grp)
+
+    prices = df[price_col].values.tolist()
+    actions = df["actions"].values.tolist()
+
+    x, y, hue = generate_points(prices, actions)
+
+    sns.pointplot(x=x, y=y, hue=hue, palette=colors, linestyles="")
+
+    sns.lineplot(data=df[price_col], color="black")
+
+    profit = calculate_profit(prices, actions)
+
+    plt.title(f"Ticker: {grp['ticker']}, Profit: {profit}")
+    plt.plot()
+
+plt.savefig("test.pdf", bbox_inches="tight")
+
+# plt.show()
