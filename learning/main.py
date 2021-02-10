16e2ba0db51c526992f0bdf9752cc612c022b33f
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-02-10 13:46:53 +0100

    Merged training and evaluation into one file

--- learning/main.py
+++ learning/main.py
@@ -1,51 +1,87 @@
+import warnings
+
 from learning.env import StockEnv
 from learning.agent import Agent
 import paths
 import pickle as pkl
 from random import shuffle
 
-with open(paths.data_path / "data_timeseries.pkl", "rb") as f:
-    data = pkl.load(f)
-shuffle(data)
 
-env = StockEnv()
+def main(input_path, eval=False, model_path=None, eval_out_path=None):
+    if eval:
+        warnings.warn("Eval is active.")
 
-state_size = data[0]["data"].shape[1] - 2  # -1 because we remove the "Close" and "tradeable" column
-agent = Agent(state_size=state_size, action_size=3, memory_len=1000)
+        if model_path is None or eval_out_path is None:
+            raise ValueError("Eval is activated but no model or eval out path is specified.")
 
-n_episodes = 3
-batch_size = 32
+        eval_out_path = model_path / eval_out_path
 
-for i, grp in enumerate(data):
+    with open(input_path, "rb") as f:
+        data = pkl.load(f)
+    shuffle(data)
 
-    print(f"{i}/{len(data)} - Processing ticker: {grp['ticker']}")
+    env = StockEnv()
+    state_size = data[0]["data"].shape[1] - 2  # -1 because we remove the "Close" and "tradeable" column
+    agent = Agent(state_size=state_size, action_size=3, memory_len=1000)
 
-    df = grp["data"].drop(columns=["Close", "tradeable"])
+    n_episodes = 3
+    batch_size = 32
 
-    for e in range(n_episodes):
+    if eval:
+        agent.load(model_path)
+        n_episodes = 1
+        batch_size = 0
 
-        print(f"Episode {e}/{n_episodes}")
+    for i, grp in enumerate(data):
 
-        state = env.reset(df)
-        done = False
+        print(f"{i}/{len(data)} - Processing ticker: {grp['ticker']}")
 
-        while not done:
+        df = grp["data"].drop(columns=["Close", "tradeable"])
 
-            action = agent.act(state)
+        if eval:
+            actions = []
 
-            next_state, reward, done, _ = env.step(action)
+        for e in range(n_episodes):
 
-            if done:
-                break
+            print(f"Episode {e}/{n_episodes}")
 
-            agent.remember(state, action, reward, next_state, done)
+            state = env.reset(df)
+            done = False
 
-            state = next_state
+            while not done:
 
-        if len(agent.memory) > batch_size:
-            # Note that agent.memory is a queue and we do not delete elements when replaying. Therefore, yes, we will
-            # replay on the first loop when agent.memory == batch_size BUT we do not delete the content of the queue.
-            # So it grows and eventually will throw out "old" state/action pairs.
-            agent.replay(batch_size)
+                action = agent.act(state)
 
-agent.save(paths.models_path)
+                if eval:
+                    actions.append(action)
+
+                next_state, reward, done, _ = env.step(action)
+
+                if done:
+                    break
+
+                if not eval:
+                    agent.remember(state, action, reward, next_state, done)
+
+                state = next_state
+
+            if not eval and len(agent.memory) > batch_size:
+                # Note that agent.memory is a queue and we do not delete elements when replaying. Therefore, yes, we will
+                # replay on the first loop when agent.memory == batch_size BUT we do not delete the content of the queue.
+                # So it grows and eventually will throw out "old" state/action pairs.
+                agent.replay(batch_size)
+
+        if eval:
+            grp["data"]["actions"] = actions + [-1]  # -1 since we do not have an action for the last entry
+
+    if eval:
+        with open(eval_out_path, "wb") as f:
+            pkl.dump(data, f)
+    else:
+        agent.save(paths.models_path)
+
+
+if __name__ == "__main__":
+    main(paths.train_path / "data_timeseries.pkl",
+         eval=True, model_path=paths.models_path / "18_44---08_02-21.mdl",
+         eval_out_path="eval.pkl")
