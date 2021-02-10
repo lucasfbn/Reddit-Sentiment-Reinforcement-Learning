e99326364bb0c733c567d3f7254fa87e6f0c5ed6
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-02-10 14:07:07 +0100

    Simulator with simple evaluate wrapper

---
+++ evaluate/simulate.py
@@ -0,0 +1,16 @@
+import learning
+from learning.env import StockEnv
+from learning.agent import Agent
+
+import paths
+import pickle as pkl
+
+
+class Simulator:
+
+    def __init__(self, data_path, model_path):
+        self.data_path = data_path
+        self.model_path = model_path
+
+    def evaluate(self):
+        learning.main.main(self.data_path, eval=True, model_path=self.model_path, eval_out_path="eval.pkl")
