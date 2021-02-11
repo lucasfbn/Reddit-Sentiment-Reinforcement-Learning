6cfd1f27ad37f28289a6550303dd53c1611610d5
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-02-11 21:44:40 +0100

    Simulate file (not needed)

--- evaluate/simulate.py
+++ evaluate/simulate.py
@@ -1,14 +1,38 @@
+import pickle as pkl
+
 import learning
+import paths
+from evaluate.eval_all_trades import EvaluateAllTrades
 
 
 class Simulator:
 
-    def __init__(self, data_path, model_path):
-        self.data_path = data_path
+    def __init__(self, model_path):
         self.model_path = model_path
 
-        self.evaluated_data = None
+        self.act_data = None
 
-    def evaluate(self):
-        self.evaluated_data = learning.main.main(self.data_path, eval=True,
-                                                 model_path=self.model_path, eval_out_path="eval.pkl")
+    def _load(self, path):
+        with open(path, "rb") as f:
+            data = pkl.load(f)
+        return data
+
+    def act(self, data_path):
+        self.act_data = learning.main.main(data_path, eval=True,
+                                           model_path=self.model_path, eval_out_path="eval.pkl")
+
+    def evaluate(self, evaluate_in=None, evaluate_out=None):
+        if evaluate_in is not None:
+            data = self._load(evaluate_in)
+        else:
+            data = self.act_data
+
+        et = EvaluateAllTrades(data, evaluate_out)
+        print(et.overall_statistics())
+
+
+
+
+s = Simulator(model_path=paths.models_path / "18_44---08_02-21.mdl")
+# s.act(paths.test_path / "data_timeseries.pkl")
+s.evaluate(paths.models_path / "18_44---08_02-21.mdl" / "eval.pkl")
