8c0006358db1c9389d5ec5cb780a5e281e2b4266
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-02-16 13:03:24 +0100

    Update paths.py

--- paths.py
+++ paths.py
@@ -16,5 +16,7 @@ data_paths = {
     0: data_path / "train",
     1: data_path / "test",
     2: data_path / "live",
-    3: data_path / "look_back_7_start_22_0"
+    3: data_path / "look_back_7_start_22_0_min_len_7",
+    4: data_path / "look_back_7_start_22_0_min_len_9"
+
 }
