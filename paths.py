82103fe52d456b9ff244a3c19e4278136cc9f9ce
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-02-16 12:57:04 +0100

    Added path dict for easier access

--- paths.py
+++ paths.py
@@ -11,3 +11,10 @@ models_path = base_path / "models"
 train_path = data_path / "train"
 test_path = data_path / "test"
 live_path = data_path / "live"
+
+data_paths = {
+    0: data_path / "train",
+    1: data_path / "test",
+    2: data_path / "live",
+    3: data_path / "look_back_7_start_22_0"
+}
