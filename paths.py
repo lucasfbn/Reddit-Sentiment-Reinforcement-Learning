37ac57f76f195b1a27dacc287175f5fcbfcae71b
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-02-16 13:43:37 +0100

    Update paths.py

--- paths.py
+++ paths.py
@@ -8,15 +8,11 @@ base_path = Path(__file__).parent
 data_path = base_path / "data"
 models_path = base_path / "models"
 
-train_path = data_path / "train"
-test_path = data_path / "test"
-live_path = data_path / "live"
-
+data_overview = data_path / "overview.csv"
 data_paths = {
     0: data_path / "train",
     1: data_path / "test",
     2: data_path / "live",
-    3: data_path / "look_back_7_start_22_0_min_len_7",
-    4: data_path / "look_back_7_start_22_0_min_len_9"
-
+    3: data_path / "3",
+    4: data_path / "4"
 }
