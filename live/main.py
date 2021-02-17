7a499ce9648bfe3614d7c1bf8574e73febeb4dec
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-02-17 20:34:54 +0100

    Script for live preprocessing

---
+++ live/main.py
@@ -0,0 +1,30 @@
+import paths
+from preprocess.preprocessor import Preprocessor
+from preprocess.merge_hype_price import MergeHypePrice
+from preprocess.cleaner import Cleaner
+from preprocess.timeseries_generator import TimeseriesGenerator
+
+Preprocessor.path = paths.data_paths[6]
+
+mhp = MergeHypePrice(
+    start_hour=22,
+    start_min=0,
+    market_symbols=[],
+    min_len_hype=1,
+    start_offset=30,
+    live=True,
+    limit=None
+)
+mhp.pipeline()
+
+c = Cleaner(
+    min_len_hype_price=1,
+    keep_offset=7
+)
+c.pipeline()
+
+tsg = TimeseriesGenerator(
+    look_back=7,
+    live=True
+)
+tsg.pipeline()
