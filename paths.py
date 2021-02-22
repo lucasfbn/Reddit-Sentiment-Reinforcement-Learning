3cdd3bccc806df8e1473c00dfea837c412762c99
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-02-22 16:49:33 +0100

    Merged paths

--- paths.py
+++ paths.py
@@ -14,3 +14,9 @@ tracking_path = storage_path / "tracking"
 
 def d_path(folder):
     return data_path / str(folder)
+
+
+# sentiment analysis
+sentiment_analysis_path = base_path / "preprocess" / "sentiment_analysis"
+ticker_folder = sentiment_analysis_path / "analyze" / "ticker" / "files"
+all_ticker = sentiment_analysis_path / "analyze" / "ticker" / "ticker.csv"
