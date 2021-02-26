18b7ab2107911b3ebefaaf46140e3b0e36660f9a
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-02-26 09:46:09 +0100

    Updated api

--- preprocess/sentiment_analysis/api/pushshift.py
+++ preprocess/sentiment_analysis/api/pushshift.py
@@ -6,35 +6,7 @@ import requests
 from preprocess.sentiment_analysis.api.google_cloud import BigQueryDB
 from psaw import PushshiftAPI
 
-from utils import dt_to_timestamp, log
-
-submission_schema = {"created_utc": "int64", "author": "object", "id": "object", "title": "object",
-                     "subreddit": "object", "num_comments": "int64", "selftext": "object",
-                     "score": "int64", "upvote_ratio": "float64", "full_link": "object", "author_fullname": "object",
-                     "allow_live_comments": "bool", "author_flair_css_class": "object",
-                     "author_flair_richtext": "object", "author_flair_text": "object", "author_flair_type": "object",
-                     "author_patreon_flair": "object", "author_premium": "object", "awarders": "object",
-                     "can_mod_post": "bool", "contest_mode": "bool", "domain": "object",
-                     "gildings": "object", "is_crosspostable": "bool", "is_meta": "bool",
-                     "is_original_content": "bool", "is_reddit_media_domain": "bool", "is_robot_indexable": "bool",
-                     "is_self": "bool", "is_video": "bool", "link_flair_background_color": "object",
-                     "link_flair_css_class": "object", "link_flair_richtext": "object",
-                     "link_flair_template_id": "object", "link_flair_text": "object",
-                     "link_flair_text_color": "object", "link_flair_type": "object", "locked": "bool",
-                     "media_only": "bool", "no_follow": "bool", "num_crossposts": "int64", "over_18": "bool",
-                     "parent_whitelist_status": "object", "permalink": "object", "pinned": "bool",
-                     "post_hint": "object", "removed_by_category": "object", "retrieved_on": "int64",
-                     "send_replies": "bool", "spoiler": "bool", "stickied": "bool",
-                     "subreddit_id": "object", "subreddit_subscribers": "int64", "subreddit_type": "object",
-                     "suggested_sort": "object", "thumbnail": "object", "thumbnail_height": "float64",
-                     "thumbnail_width": "float64", "total_awards_received": "int64", "treatment_tags": "object",
-                     "url": "object", "url_overridden_by_dest": "object",
-                     "whitelist_status": "object", "created": "float64",
-                     "author_flair_background_color": "object", "author_flair_text_color": "object", "media": "object",
-                     "media_embed": "object", "secure_media": "object", "secure_media_embed": "object",
-                     "is_gallery": "object", "gallery_data": "object", "author_flair_template_id": "object",
-                     "author_cakeday": "object", "banned_by": "object", "distinguished": "object", "edited": "float64",
-                     "gilded": "object", "collections": "object"}
+from utils import dt_to_timestamp, log, submission_schema
 
 
 class MainApi:
@@ -78,6 +50,9 @@ class MainApi:
 class BetaAPI:
     # Used for current data
 
+    def __init__(self):
+        self.submissions = None
+
     def submission_request(self, subreddit):
         baseurl = "https://beta.pushshift.io/search/reddit/submissions"
         params = {
@@ -97,26 +72,37 @@ class BetaAPI:
 
         return response.json()["data"]
 
-    def filter_early_submissions(self, submissions, start):
+    def filter_early_submissions(self, start):
         filtered_submissions = []
-        for submission in submissions:
+        for submission in self.submissions:
             if submission["created_utc"] >= start:
                 filtered_submissions.append(submission)
 
-        return filtered_submissions
+        self.submissions = filtered_submissions
 
-    def _extract_ids(self, submissions):
-        ids = []
-        for subm in submissions:
-            ids.append(subm["id"])
-        return ids
+    def _extract_relevant_data(self):
+        new_submissions = []
+        for subm in self.submissions:
+            temp = {}
+            for schema_key in submission_schema:
+                if schema_key in subm:
+                    temp[schema_key] = subm[schema_key]
+                else:
+                    temp[schema_key] = None
+            new_submissions.append(temp)
+        self.submissions = new_submissions
 
-    def get_submissions_ids(self, start, subreddit):
+    def _to_df(self):
+        df = pd.DataFrame(self.submissions)
+        df = df.astype(submission_schema)
+        return df
+
+    def get_submissions(self, start, subreddit):
         after = dt_to_timestamp(start)
-        submissions = self.submission_request(subreddit)
-        submissions = self.filter_early_submissions(submissions, start)
-        submission_ids = self._extract_ids(submissions)
-        return submission_ids
+        self.submission_request(subreddit)
+        self.filter_early_submissions(start)
+        df = self._to_df()
+        return df
 
 
 def download():
@@ -144,11 +130,13 @@ def download():
 
 
 if __name__ == "__main__":
-    # p = BetaAPI()
-    # res = p.get_submissions_ids(start=1614272760, subreddit="wallstreetbets")
+    start = datetime(year=2021, month=2, day=23, hour=21)
+
+    p = BetaAPI()
+    res = p.get_submissions(start=start, subreddit="wallstreetbets")
 
     # p = MainApi()
     # df = p.get_submissions(start=1614067200, end=1614088800, subreddit="stocks")
     # print()
 
-    download()
+    # download()
--- preprocess/sentiment_analysis/api/reddit.py
+++ preprocess/sentiment_analysis/api/reddit.py
@@ -1,35 +1,70 @@
 import praw
+from utils import dt_to_timestamp, log, submission_schema
+import pandas as pd
 
 
-class Downloader:
+class API:
 
-    def __init__(self, user_agent, client_id, client_secret, username, password, subreddit, begin):
-        self.api = praw.Reddit(user_agent=user_agent,
-                               client_id=client_id,
-                               client_secret=client_secret,
-                               username=username,
-                               password=password)
-        self.subreddit = subreddit
-        self.begin = begin
+    def __init__(self, user_agent, client_id, client_secret, username, password):
+        self.reddit = praw.Reddit(user_agent=user_agent,
+                                  client_id=client_id,
+                                  client_secret=client_secret,
+                                  username=username,
+                                  password=password)
+        self.submissions = []
 
-    def download(self):
-        sub = self.api.subreddit(self.subreddit)
-        submissions = sub.new()
+    def get_latest_submissions(self, start, subreddit, limit=10):
+        sub = self.reddit.subreddit(subreddit)
+        sub_submissions = sub.new(limit=limit)
 
-        for subm in submissions:
-            print()
+        submissions = []
+
+        for subm in sub_submissions:
+            if subm.created_utc < start:
+                break
+            subm = Submission(subm)
+            submissions.append(subm.extract_relevant_data())
+
+        self.submissions = submissions
+
+    def get_submissions_by_id(self, ids):
+
+        # Add reddit prefixes for objects, see: https://www.reddit.com/dev/api/
+        ids = [f"t3_{id}" for id in ids]
+
+        submissions = []
+
+        for subm in self.reddit.info(fullnames=ids):
+            subm = Submission(subm)
+            submissions.append(subm.extract_relevant_data())
+
+        self.submissions = submissions
+
+    def to_df(self):
+        df = pd.DataFrame(self.submissions)
+        df = df.astype(submission_schema)
+        return df
 
 
 class Submission:
 
-    def __init__(self, submission_data):
-        self.submission_data = submission_data
+    def __init__(self, submission):
+        self.submission = submission
 
-    def _extract_relevant(self):
-        pass
-
-    def to_df(self):
-        pass
+    def extract_relevant_data(self):
+        temp = {}
+        submission_dict = self.submission.__dict__
+        for schema_key in submission_schema:
+            if schema_key in submission_dict:
+                if schema_key == "author":
+                    temp[schema_key] = self.submission.author.name
+                elif schema_key == "subreddit":
+                    temp[schema_key] = self.submission.subreddit.display_name
+                else:
+                    temp[schema_key] = submission_dict[schema_key]
+            else:
+                temp[schema_key] = None
+        return temp
 
 
 if __name__ == '__main__':
@@ -39,6 +74,9 @@ if __name__ == '__main__':
     username = "qrzte"
     password = "ga5FwRaXcRyJ77e"
 
-    d = Downloader(user_agent=user_agent, client_id=client_id, client_secret=client_secret,
-                   username=username, password=password, subreddit="wallstreetbets", begin=1)
-    d.download()
+    d = API(user_agent=user_agent, client_id=client_id, client_secret=client_secret,
+            username=username, password=password)
+    d.get_latest_submissions(start=1, subreddit="wallstreetbets")
+    d.get_submissions_by_id(ids=["ls36lf", "lrlbh5"])
+    df = d.to_df()
+    print()
