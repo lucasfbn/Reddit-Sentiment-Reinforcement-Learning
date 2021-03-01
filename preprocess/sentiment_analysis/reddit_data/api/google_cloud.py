31aef9eb916ff29895ec63ecbe855c1b321b8edc
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-03-01 12:02:43 +0100

    implemented big query gap finder method and removed it from the new dataset generator

--- preprocess/sentiment_analysis/reddit_data/api/google_cloud.py
+++ preprocess/sentiment_analysis/reddit_data/api/google_cloud.py
@@ -1,3 +1,5 @@
+import json
+
 import pandas as pd
 
 from datetime import datetime
@@ -17,15 +19,76 @@ class BigQueryDB:
                   project_id=self.project_id,
                   if_exists="append")
 
-    def download(self, start, end, fields):
+    def download(self, start, end, fields, sql=None):
         start, end = dt_to_timestamp(start), dt_to_timestamp(end)
 
-        sql = f"""
-        SELECT {", ".join(fields)} FROM `redditdata-305217.data.submissions`
-        WHERE created_utc BETWEEN {start} AND {end}
-        """
+        if sql is None:
+            sql = f"""
+            SELECT {", ".join(fields)} FROM `redditdata-305217.data.submissions`
+            WHERE created_utc BETWEEN {start} AND {end}
+            """
 
         return pd.read_gbq(sql, project_id=self.project_id)
+
+    def detect_gaps(self, start=None, end=None, save_json=True):
+        start, end = dt_to_timestamp(start), dt_to_timestamp(end)
+
+        sql = f"""SELECT created_utc FROM `redditdata-305217.data.submissions`"""
+
+        if start is not None and end is not None:
+            sql += f" WHERE created_utc BETWEEN {start} AND {end}"
+        elif start is not None and end is None:
+            sql += f" WHERE created_utc >= {start}"
+        elif end is not None and start is None:
+            sql += f" WHERE created_utc <= {end}"
+
+        df = self.download(None, None, None, sql)
+
+        # df.to_csv("gaps.csv", sep=";", index=False)
+        # df = pd.read_csv("gaps.csv", sep=";")
+
+        date_full = pd.to_datetime(df["created_utc"], unit="s")
+        date_full = date_full.dt.tz_localize("UTC")
+        date_mesz = date_full.dt.tz_convert("Europe/Berlin")
+        df["start"] = date_mesz.dt.to_period('H').dt.to_timestamp()
+        date_list = df["start"].tolist()
+
+        min_date = df["start"].min()
+        max_date = df["start"].max()
+
+        daterange = pd.date_range(start=min_date, end=max_date - pd.Timedelta(hours=1), freq="H")
+        daterange = daterange.tolist()
+
+        difference = list(set(daterange) - set(date_list))
+        difference = pd.Series(difference)
+        difference = difference.sort_values()
+        difference = difference.tolist()
+
+        last_diff = None
+        periods = []
+        sub_period = []
+        for diff in difference:
+            if last_diff is None:
+                last_diff = diff
+                sub_period.append(diff)
+            else:
+                current_diff = diff - last_diff
+                if current_diff.seconds > 3600:
+                    periods.append(sub_period)
+                    sub_period = [diff]
+                else:
+                    sub_period.append(diff)
+            last_diff = diff
+
+        min_max_subperiod = [{"start": start, "end": end, "min_date": str(min_date), "max_date": str(max_date)}]
+        for sub_period in periods:
+            min_max_subperiod.append({"min": str(sub_period[0]), "max": str(sub_period[len(sub_period) - 1])})
+
+        if save_json:
+            with open("gaps.json", "w+") as f:
+                json.dump(min_max_subperiod, f)
+        else:
+            print(min_max_subperiod)
 
 
 if __name__ == '__main__':
@@ -36,5 +99,5 @@ if __name__ == '__main__':
     # df.to_csv("raw.csv", sep=";", index=False)
 
     db = BigQueryDB()
-    db.auth()
+    db.detect_gaps()
     # db.upload(pd.DataFrame({"one": [1, 2, 3]}), dataset="data", table="test_subm1")
