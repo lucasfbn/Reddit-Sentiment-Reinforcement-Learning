27530db39792a89ec2354ea28edebec40a3608bb
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-03-10 18:10:03 +0100

    Refactored: preprocess -> preprocessing

---
+++ preprocessing/main.py
@@ -0,0 +1,40 @@
+import paths
+from preprocess.preprocessing_utils.cleaner import Cleaner
+from preprocess.preprocessing_utils.merge_hype_price import MergePreprocessing
+from preprocess.preprocessing_utils.preprocessor import Preprocessor
+from preprocess.preprocessing_utils.timeseries_generator import TimeseriesGeneratorCNN
+from utils import tracker
+
+Preprocessor.source_path = paths.sentiment_data_path / "28-01-21 - 04-02-21_0"
+Preprocessor.target_path = paths.create_dir(paths.datasets_data_path)
+# Preprocessor.target_path = paths.datasets_data_path / "_4"
+
+tracker.add({"source_path": str(Preprocessor.source_path.name),
+             "target_path": str(Preprocessor.target_path.name)}, "Main")
+
+mhp = MergePreprocessing(
+    start_hour=21,
+    start_min=0,
+    market_symbols=[],
+    min_len_hype=1,
+    start_offset=30,
+    fill_gaps=False,
+    scale_cols_daywise=False,
+    live=False,
+    limit=None
+)
+mhp.pipeline()
+
+c = Cleaner(
+    min_len_hype_price=1,
+    keep_offset=7
+)
+c.pipeline()
+#
+tsg = TimeseriesGeneratorCNN(
+    look_back=7,
+    scale=True
+)
+tsg.pipeline()
+
+tracker.new(kind="datasets")
