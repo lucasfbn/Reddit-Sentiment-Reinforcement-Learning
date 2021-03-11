ad060c89d22423af672b2c7ea70a73c3f6965bc3
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-03-11 19:22:30 +0100

    Added config file instead of tracker

---
+++ preprocessing/config.py
@@ -0,0 +1,34 @@
+import paths
+from preprocessing.preprocessing_utils.timeseries_generator import TimeseriesGeneratorCNN
+from utils import save_config
+
+general = dict(
+    min_len=3,
+    source_path=paths.sentiment_data_path / "13-01-21 - 25-01-21_0",
+    # target_path=paths.create_dir(paths.datasets_data_path),
+    target_path=paths.datasets_data_path / "_6"
+)
+
+merge_preprocessing = dict(
+    start_hour=21,
+    start_min=0,
+    market_symbols=[],
+    start_offset=30,
+    fill_gaps=False,
+    scale_cols_daywise=False,
+    live=False,
+    limit=None
+)
+
+cleaner = dict(
+    keep_offset=7
+)
+
+timeseries_generator = dict(
+    kind=TimeseriesGeneratorCNN,
+    look_back=7,
+    scale=True
+)
+
+
+save_config(configs=[general, merge_preprocessing, cleaner, timeseries_generator], kind="dataset")
