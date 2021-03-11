e23031f093ac7ef5e37c998010f035cd870c032e
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-03-11 20:10:48 +0100

    Simple Config class to wrap the SimpleNamespace class

--- preprocessing/config.py
+++ preprocessing/config.py
@@ -1,16 +1,15 @@
-from types import SimpleNamespace
-
+from utils import Config
 import paths
 from preprocessing.preprocessing_utils.timeseries_generator import TimeseriesGeneratorCNN
 
-general = SimpleNamespace(**dict(
+general = Config(**dict(
     min_len=3,
     source_path=paths.sentiment_data_path / "13-01-21 - 25-01-21_0",
     # target_path=paths.create_dir(paths.datasets_data_path),
     target_path=paths.datasets_data_path / "_6"
 ))
 
-merge_preprocessing = SimpleNamespace(**dict(
+merge_preprocessing = Config(**dict(
     start_hour=21,
     start_min=0,
     market_symbols=[],
@@ -21,11 +20,11 @@ merge_preprocessing = SimpleNamespace(**dict(
     limit=None
 ))
 
-cleaner = SimpleNamespace(**dict(
+cleaner = Config(**dict(
     keep_offset=7
 ))
 
-timeseries_generator = SimpleNamespace(**dict(
+timeseries_generator = Config(**dict(
     kind=TimeseriesGeneratorCNN,
     look_back=7,
     scale=True
