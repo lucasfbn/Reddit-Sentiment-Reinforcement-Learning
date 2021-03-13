1de697baccad2e3cdf66f56660f98048104d8dc0
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-03-13 09:05:05 +0100

    Bulk config

--- learning/config.py
+++ learning/config.py
@@ -2,20 +2,60 @@ import paths
 from utils import Config
 from learning.main import main
 
-general = Config(**dict(
-    data_path=paths.datasets_data_path / "_13" / "timeseries.pkl",
-    kind="CNN",
-    eval=True,
-    model_path=paths.models_path / "10-52 12_03-21"
-))
+# general = Config(**dict(
+#     data_path=paths.eval_data_path / "21-50 04_03-21.pkl",
+#     kind="CNN",
+#     eval=False,
+#     model_path=None
+# ))
+#
+# agent = Config(**dict(
+#     gamma=0.95,
+#     epsilon=1.0,
+#     epsilon_decay=0.999,
+#     epsilon_min=0.15,
+#     randomness=True,
+#     memory_len=1000,
+# ))
 
-agent = Config(**dict(
+configs = [[Config(**dict(
+    data_path=paths.eval_data_path / "21-50 04_03-21.pkl",
+    kind="CNN",
+    eval=False,
+    model_path=None
+)), Config(**dict(
     gamma=0.95,
     epsilon=1.0,
     epsilon_decay=0.999,
     epsilon_min=0.15,
     randomness=True,
     memory_len=1000,
-))
+))],
+           [Config(**dict(
+               data_path=paths.eval_data_path / "21-50 04_03-21.pkl",
+               kind="CNN",
+               eval=False,
+               model_path=None
+           )), Config(**dict(
+               gamma=0.95,
+               epsilon=1.0,
+               epsilon_decay=0.999,
+               epsilon_min=0.15,
+               randomness=False,
+               memory_len=1000,
+           ))]
 
-# main(config)
+           ]
+
+new_configs = []
+for c in configs:
+    new_configs.append(c * 3)
+
+# configs = [(general, agent)]
+
+for nc in new_configs:
+    config = Config(**dict(
+        general=nc[0],
+        agent=nc[1]
+    ))
+    main(config)
