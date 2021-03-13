bc473e24a3e667e84ca08146a375d089e7c6a3a5
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-03-13 20:03:55 +0100

    Config with option for several configs

--- learning/config.py
+++ learning/config.py
@@ -1,62 +1,93 @@
 import paths
 from utils import Config
-from learning.main import main
+import copy
 
-# general = Config(**dict(
-#     data_path=paths.eval_data_path / "21-50 04_03-21.pkl",
-#     kind="CNN",
-#     evaluate=False,
-#     model_path=None
-# ))
-#
-# agent = Config(**dict(
-#     gamma=0.95,
-#     epsilon=1.0,
-#     epsilon_decay=0.999,
-#     epsilon_min=0.15,
-#     randomness=True,
-#     memory_len=1000,
-# ))
-
-configs = [[Config(**dict(
-    data_path=paths.eval_data_path / "21-50 04_03-21.pkl",
+general = Config(**dict(
+    data_path=paths.datasets_data_path / "_1" / "timeseries.pkl",
     kind="CNN",
     evaluate=False,
-    model_path=None
-)), Config(**dict(
+    model_path=paths.models_path / "14-09 13_03-21"
+))
+
+agent = Config(**dict(
     gamma=0.95,
     epsilon=1.0,
-    epsilon_decay=0.999,
-    epsilon_min=0.15,
+    epsilon_decay=0.975,
+    epsilon_min=0.10,
     randomness=True,
     memory_len=1000,
-))],
-           [Config(**dict(
-               data_path=paths.eval_data_path / "21-50 04_03-21.pkl",
-               kind="CNN",
-               evaluate=False,
-               model_path=None
-           )), Config(**dict(
-               gamma=0.95,
-               epsilon=1.0,
-               epsilon_decay=0.999,
-               epsilon_min=0.15,
-               randomness=False,
-               memory_len=1000,
-           ))]
+))
 
-           ]
+model = Config(**dict(
+    n_episodes=3,
+    batch_size=32
+))
 
-new_configs = []
-for c in configs:
-    new_configs.append(c)
-    new_configs.append(c)
+config = Config(**dict(general=general, agent=agent, model=model))
 
-# configs = [(general, agent)]
-
-for nc in new_configs:
-    config = Config(**dict(
-        general=nc[0],
-        agent=nc[1]
-    ))
-    main(config)
+configs = [
+    [Config(**dict(
+        data_path=paths.datasets_data_path / "_13" / "timeseries.pkl",
+        kind="CNN",
+        evaluate=False,
+        model_path=None
+    )), Config(**dict(
+        gamma=0.95,
+        epsilon=1.0,
+        epsilon_decay=0.999,
+        epsilon_min=0.15,
+        randomness=True,
+        memory_len=1000,
+    )), Config(**dict(
+        n_episodes=3,
+        batch_size=32
+    ))],
+    [Config(**dict(
+        data_path=paths.datasets_data_path / "_13" / "timeseries.pkl",
+        kind="CNN",
+        evaluate=False,
+        model_path=None
+    )), Config(**dict(
+        gamma=0.95,
+        epsilon=1.0,
+        epsilon_decay=0.999,
+        epsilon_min=0.15,
+        randomness=False,
+        memory_len=1000,
+    )), Config(**dict(
+        n_episodes=3,
+        batch_size=32
+    ))],
+    [Config(**dict(
+        data_path=paths.datasets_data_path / "_2" / "timeseries.pkl",
+        kind="CNN",
+        evaluate=False,
+        model_path=None
+    )), Config(**dict(
+        gamma=0.95,
+        epsilon=1.0,
+        epsilon_decay=0.999,
+        epsilon_min=0.15,
+        randomness=True,
+        memory_len=1000,
+    )), Config(**dict(
+        n_episodes=3,
+        batch_size=32
+    ))],
+    [Config(**dict(
+        data_path=paths.datasets_data_path / "_2" / "timeseries.pkl",
+        kind="CNN",
+        evaluate=False,
+        model_path=None
+    )), Config(**dict(
+        gamma=0.95,
+        epsilon=1.0,
+        epsilon_decay=0.999,
+        epsilon_min=0.15,
+        randomness=False,
+        memory_len=1000,
+    )), Config(**dict(
+        n_episodes=3,
+        batch_size=32
+    ))],
+]
