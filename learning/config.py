1b07fb51221ae51c449d406b0ae19240b865d327
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-03-13 08:51:11 +0100

    Implemented hard settings as instance variables, implemented new variable "randomness" to enable/disable exploration

--- learning/config.py
+++ learning/config.py
@@ -2,11 +2,20 @@ import paths
 from utils import Config
 from learning.main import main
 
-config = Config(**dict(
+general = Config(**dict(
     data_path=paths.datasets_data_path / "_13" / "timeseries.pkl",
     kind="CNN",
     eval=True,
     model_path=paths.models_path / "10-52 12_03-21"
 ))
 
-main(config)
+agent = Config(**dict(
+    gamma=0.95,
+    epsilon=1.0,
+    epsilon_decay=0.999,
+    epsilon_min=0.15,
+    randomness=True,
+    memory_len=1000,
+))
+
+# main(config)
