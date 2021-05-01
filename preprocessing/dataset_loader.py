29059840b0892d3fddbc28797855c1ee97e5b5b6
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-05-01 22:17:25 +0200

    Implemented option to load cnn or nn data

--- preprocessing/dataset_loader.py
+++ preprocessing/dataset_loader.py
@@ -6,21 +6,26 @@ import paths
 
 
 class DatasetLoader:
+    kind_dict = {"nn": "nn_input.pkl", "cnn": "cnn_input.pkl"}
 
-    def __init__(self, run_ids: list):
+    def __init__(self, run_ids: list, kind: str):
+        if kind not in ["nn", "cnn"]:
+            raise ValueError
+
         mlflow.set_tracking_uri(paths.mlflow_path)
         mlflow.set_experiment("Datasets")
 
         self.run_ids = run_ids
 
         self.data = []
+        self.kind = kind
 
     def load(self):
         for run_id in self.run_ids:
             from_run = mlflow.get_run(run_id)
             from_artifact_path = paths.artifact_path(from_run.info.artifact_uri)
 
-            with open(from_artifact_path / "timeseries.pkl", "rb") as f:
+            with open(from_artifact_path / self.kind_dict[self.kind], "rb") as f:
                 self.data.append(pkl.load(f))
         return self.data
 
@@ -35,8 +40,7 @@ if __name__ == "__main__":
     mlflow.set_tracking_uri(paths.mlflow_path)
     mlflow.set_experiment("Datasets")
 
-    run_ids = ["58d6faa3746b46b7839f62fcb03239ea", "82e8310bd4134c45a071ce6d5175b297"]
-    dl = DatasetLoader(run_ids)
-    a = dl.load()
-    merged = dl.merge()
+    run_ids = ["3bf57f5ad0d94c0f8ab0848438b78808"]
+    dl = DatasetLoader(run_ids, "cnn").merge()
+
     print()
