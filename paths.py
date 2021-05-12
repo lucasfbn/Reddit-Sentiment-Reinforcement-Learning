95a5fb4e2c7de9e01c63c22db6fcec9b705c9913
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-05-12 18:06:54 +0200

    Implemented cache for stock downloading. Closes #19

--- paths.py
+++ paths.py
@@ -12,6 +12,7 @@ data_path = storage_path / "data"
 mlflow_path = f"file:///{(storage_path / 'mlflow' / 'mlruns').as_posix()}"
 artifact_path = lambda artifact_uri: Path("C:" + artifact_uri.split(":")[2])
 
+stock_cache = storage_path / "stock_cache" / "yfinance_cache"
 
 def create_dir(path, fn="", suffix=0):
     while os.path.exists(path / (fn + f"_{suffix}")):
