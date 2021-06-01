40a0c1bb64be5885d870cfa2b5d4920e910f0985
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-06-01 18:59:51 +0200

    Overhaul of sentiment analysis
    Implemented prefect framework
    closes #27

---
+++ sentiment_analysis/logic/analyzer.py
@@ -0,0 +1,439 @@
+from datetime import datetime
+
+import pandas as pd
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from prefect import task
+
+import paths
+from mlflow_api import log_file
+from sentiment_analysis.logic.timespan import Timespan
+from sentiment_analysis.reddit_data.api.google_cloud import BigQueryDB
+
+
+@task
+def mlflow_log_file(obj, fn):
+    log_file(obj, fn)
+
+
+@task
+def get_from_gc(start: datetime, end: datetime, check_duplicates: bool, fields_to_retrieve: list) -> pd.DataFrame:
+    """
+    Downloads the raw data from GC.
+
+    Args:
+        start: Start date
+        end: End date
+        check_duplicates: Whether to drop duplicates or not
+        fields_to_retrieve: Subset of all possible fields
+
+    Returns:
+        Data as DataFrame
+    """
+    db = BigQueryDB()
+    df = db.download(start=start, end=end, fields=fields_to_retrieve, check_duplicates=check_duplicates)
+    return df
+
+
+@task
+def filter_removed(df: pd.DataFrame, cols_to_check_if_removed: list) -> pd.DataFrame:
+    """
+    Checks if the submission got removed and filters it if it was.
+
+    Args:
+        df: DataFrame
+        cols_to_check_if_removed: List of column which are inspected for certain deletion-specific keywords
+
+    Returns:
+        Filtered DataFrame
+    """
+    for col in cols_to_check_if_removed:
+        df = df[~df[col].isin(["[removed]", "[deleted]"])]
+    return df
+
+
+@task
+def add_temporal_informations(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds several informations to the each submission in the DF.
+
+    - converts date to MESZ (timezone), and replace the default date (the origin column "created_utc" is in UTC format)
+    - keeps only the day part of the date from above (so, hours etc are ignored - useful when filtering for stuff that
+      happened within one day)
+    - start (also as timestamp)
+    - end (also as timestamp)
+
+    For start and end we refer to the timespan (hour) in which the submission was created.
+    For instance: date_mesz: 10/10/2021 06:15
+        => start = 10/10/2021 06:00, end = 10/10/2021 07:00
+
+    This is useful/necessary since we will later aggregate the data based on each hour of the covered timeperiod.
+    E.g. if we want to work with the data from 01.01.2021 00:00 until 02.01.2021 00:00 we will have 24 start and end
+    periods.
+
+    Args:
+        df:
+
+    Returns:
+
+    """
+
+    def _get_unix_timestamp(date):
+        return (date - pd.Timestamp("1970-01-01")) // pd.Timedelta("1s")
+
+    date = pd.to_datetime(df["created_utc"], unit="s")
+    date = date.dt.tz_localize("UTC")
+    df["date"] = date.dt.tz_convert("Europe/Berlin")
+    df["date_day"] = df["date"].dt.to_period("D")
+
+    start = df["date"].dt.to_period('H').dt.to_timestamp()
+    df["start"] = start
+    df["start_timestamp"] = _get_unix_timestamp(start)
+
+    end = (df["date"] + pd.Timedelta(seconds=3600)).dt.to_period('H').dt.to_timestamp()
+    df["end"] = end
+    df["end_timestamp"] = _get_unix_timestamp(end)
+    return df
+
+
+@task
+def filter_authors(df: pd.DataFrame, filter_too_frequent_authors: bool, author_blacklist: list,
+                   max_submissions_per_author_per_day: int) -> pd.DataFrame:
+    """
+    Filters submissions based on a given number of allowed submission per author per day.
+
+    Args:
+        df:
+        filter_too_frequent_authors: Whether to apply this filter or not
+        author_blacklist: List of author names whose submissions are filtered by default
+        max_submissions_per_author_per_day: The max number of submissions per author per day
+
+    Returns:
+
+    """
+
+    if not filter_too_frequent_authors:
+        return df
+
+    filtered_rows = []
+
+    cols = df.columns
+
+    # Group by the submissions of a single day
+    days = df.groupby(["date_day"])
+
+    for day, submissions in days:
+        submissions = pd.DataFrame(submissions)
+
+        # Group by all authors within the day
+        authors = submissions.groupby(["author"])
+
+        for author_id, author_submissions in authors:
+
+            if author_id in author_blacklist:
+                continue
+
+            if len(author_submissions) <= max_submissions_per_author_per_day:
+                filtered_rows.append(author_submissions)
+            else:
+                # If the number of submissions per author per day exceeds the threshold:
+                # Sort by number of comment and only take the n-top ones
+                # (e.g. drop the rest of the submissions from the same author)
+                author_submissions = author_submissions.sort_values(by=["num_comments"], ascending=False)
+                filtered_rows.append(author_submissions.head(max_submissions_per_author_per_day))
+
+    df = pd.concat(filtered_rows)
+    df.columns = cols
+    return df
+
+
+@task
+def delete_non_alphanumeric(df, cols_to_be_cleaned: list) -> pd.DataFrame:
+    """
+    Removes all non alphanumeric characters.
+    We do NOT remove stuff like "!, ?, :), :D" since it adds value to the sentiment analysis later on.
+
+    Args:
+        df:
+        cols_to_be_cleaned: Columns to be cleaned
+
+    Returns:
+
+    """
+
+    for col in cols_to_be_cleaned:
+        df[col] = df[col].str.replace('[^\w\s,.?!()-+:"]', '', regex=True)
+    return df
+
+
+@task
+def load_valid_ticker(valid_ticker_path):
+    ticker = pd.read_csv(paths.all_ticker, sep=";")["Symbol"]
+    ticker = ticker[ticker.str.len() >= 2]
+    return ticker.values.tolist()
+
+
+def extract_ticker(txt: str, valid_ticker: list, ticker_blacklist: list):
+    """
+    Extracts (stock) ticker from a given string. Each ticker has to be in the valid_ticker list and will be ignored
+    if in the ticker_blacklist.
+
+    Args:
+        txt: str with potential ticker
+        valid_ticker: List of valid ticker
+        ticker_blacklist: List of ticker that shall be ignored (even tho they might be in valid_ticker)
+
+    Returns:
+        A list of all ticker that have been found in the txt
+    """
+
+    # TODO Make detection of ticker more precise (currently, "GME, TSLA" will not recognize "GME" for example)
+    # TODO See test
+
+    # TODO We could replace "word not in occured_ticker" by list(set(occured_ticker)) before return occured_ticker
+    # However, this changes order from time to time and thus the tests will fail. Also not sure if this improves
+    # performance. It's probably rather unimportant.
+
+    occurred_ticker = []
+    try:
+        words = txt.split(" ")
+    except (TypeError, AttributeError):
+        return None
+
+    for word in words:
+        if len(word) <= 5 and \
+                word not in ticker_blacklist and \
+                word in valid_ticker and \
+                word not in occurred_ticker:
+            occurred_ticker.append(word)
+
+    if not occurred_ticker:
+        return None
+    return occurred_ticker
+
+
+@task
+def get_submission_ticker(df: pd.DataFrame, valid_ticker: list, ticker_blacklist: list,
+                          search_ticker_in_body: bool) -> pd.DataFrame:
+    """
+    Retrieves all ticker within a submission. A potential ticker has to be in valid_ticker and not in ticker_blacklist.
+    Also, if the search should be extended to the submission body (default: only title is searched),
+    search_ticker_in_body has to be enabled.
+
+    Args:
+        df:
+        valid_ticker: List of valid ticker
+        ticker_blacklist: List of blacklisted ticker
+        search_ticker_in_body: Whether to search the submission body or not. Even if True it will only search for ticker
+         in the body if there were no ticker found in the title
+
+    Returns:
+
+    """
+    df["title_ticker"] = df.apply(lambda row: extract_ticker(row["title"], valid_ticker, ticker_blacklist),
+                                  axis="columns")
+
+    if search_ticker_in_body:
+        df["body_ticker"] = df.apply(lambda row: (extract_ticker(row["selftext"], valid_ticker, ticker_blacklist)
+                                                  if row["title_ticker"] is None else None), axis="columns")
+    else:
+        df["body_ticker"] = None
+
+    # Currently, the body of a submission shall not be searched for ticker when there was a valid ticker in the title
+    assert len(df[(df["title_ticker"].notnull()) & (df["body_ticker"].notnull())]) == 0
+
+    return df
+
+
+@task
+def filter_submissions_without_ticker(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Filters all submission without a valid ticker in either the title or the body.
+
+    Logic:
+    Filter all submissions where ¬(¬title_ticker ∧ ¬body_ticker)
+
+    Truth Table
+    body_ticker	title_ticker	¬(¬title_ticker ∧ ¬body_ticker)
+    F	F	                                    F
+    F	T	                                    T
+    T	F	                                    T
+    T	T	                                    T
+
+    (Although the last entry of the TT is currently not possible, see @get_submission_ticker)
+
+    Args:
+        df:
+
+    Returns:
+
+    """
+    df = df[~((df["title_ticker"].isnull()) & (df["body_ticker"].isnull()))]
+    return df
+
+
+@task
+def merge_ticker_to_a_single_column(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Merges the two columns "title_ticker" and "body_ticker" into one column, depending in their respective value.
+    The two columns "title_ticker" and "body_ticker" are dropped afterwards.
+
+    Examples:
+
+    | title_ticker | body_ticker | ticker |
+    |--------------|-------------|--------|
+    | GME          | None        | GME    |
+    | None         | GME         | GME    |
+
+    Args:
+        df:
+
+    Returns:
+
+    """
+    df["ticker"] = df["title_ticker"].fillna(df["body_ticker"])
+    df = df.drop(columns=["title_ticker", "body_ticker"])
+    return df
+
+
+@task
+def analyze_sentiment(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Basic sentiment analyzer. Analyses the sentiment of each **title** of each submission.
+    Currently, each submission is extended by an estimation how much of the text is positive, neutral or negative.
+    An additional compound score is issued, indicating the overall score of the text.
+
+    # TODO Analyze body, not only title.
+    # TODO Analyze comments?
+
+    Args:
+        df:
+
+    Returns:
+
+    """
+    analyzer = SentimentIntensityAnalyzer()
+
+    def _analyze(txt):
+        scores = analyzer.polarity_scores(txt)
+        return [scores["pos"], scores["neu"], scores["neg"], scores["compound"]]
+
+    df[["pos", "neu", "neg", "compound"]] = df.apply(lambda row: _analyze(row["title"]), axis="columns",
+                                                     result_type="expand")
+    return df
+
+
+@task
+def flatten_ticker_scores(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Flattens the ticker that occur in the submissions. At this point we don't care about the actual
+    submission, all we care about is the respective (vader) score and the respective ticker that belong to that text.
+    We, therefore, flatten the list of tickers and duplicate the row of a single submission for each ticker.
+
+    Example:
+
+        | ticker      | id |
+        |-------------|----|
+        | [GME, TSLA] | 1  |
+        | [GME]       | 2  |
+
+        gets flattened to:
+
+        | ticker | id |
+        |--------|----|
+        | GME    | 1  |
+        | TSLA   | 1  |
+        | GME    | 2  |
+
+    Args:
+        df:
+
+    Returns:
+        Flattened df.
+    """
+    flattened = []
+
+    def flatten(row):
+        for ticker in row["ticker"]:
+            temp = pd.DataFrame(row).T
+            temp["ticker"] = ticker
+            flattened.append(temp)
+
+    df.apply(flatten, axis="columns")
+
+    flattened_df = pd.concat(flattened)
+    return flattened_df
+
+
+@task
+def retrieve_timespans(df: pd.DataFrame, relevant_timespan_cols: list) -> list:
+    """
+    Groups the submissions/ticker into their respective start and end date periods.
+    Only keeps the columns of each submission that are specified in relevant_timespan_cols.
+
+    Args:
+        df:
+        relevant_timespan_cols: List of columns that shall not be dropped (e.g. are of use later on)
+
+    Returns:
+        List of Timespan objects
+    """
+
+    timespan_start = df.groupby("start")
+
+    timespans = []
+    for timespan, submissions in timespan_start:
+        # since the periods is the same within the grouped timespans we can assign start and end of the whole timespan
+        # from the first element in the df.
+        timespans.append(Timespan(start=submissions["start"].iloc[0],
+                                  end=submissions["end"].iloc[0],
+                                  df=submissions[relevant_timespan_cols]))
+
+    return timespans
+
+
+@task
+def aggregate_submissions_per_timespan(ts: Timespan):
+    """
+    Aggregates the data of a single timespan (1 hour atm).
+    E.g. each pos/neg/neu/compound score gets summed up for each ticker.
+    See tests for further insights.
+    We also add the number of mentions for each ticker.
+    Note: aggregating will drop all columns that are not in [int, float].
+
+    Args:
+        ts:
+
+    Returns:
+
+    """
+
+    # By adding a col where each value is one we can easily see the sum of posts that got aggregated
+    # afterwards
+    ts.df["num_posts"] = 1
+
+    ts.df = ts.df.groupby("ticker").agg("sum").reset_index()
+    return ts
+
+
+@task
+def summarize_timespans(timespans: list) -> pd.DataFrame:
+    """
+    "Summarizes" all timespans by flattening them. Each ticker in each timespan will be added to a separate DF,
+    containing the aggregated values and its start and end date.
+
+    Args:
+        timespans: List of Timespan objects
+
+    Returns:
+        Flattened DF of the timespans
+    """
+
+    dfs = []
+
+    for timespan in timespans:
+        df = timespan.df
+        df["start"] = timespan.start
+        df["end"] = timespan.end
+        dfs.append(df)
+
+    return pd.concat(dfs).reset_index(drop=True)
---
+++ tests/test_sentiment_analysis/test_logic/test_retrieve_data.py
@@ -0,0 +1,200 @@
+from pandas import Timestamp, Period
+from pandas.testing import assert_frame_equal
+
+from sentiment_analysis.logic.analyzer import *
+
+
+def test_get_from_gc():
+    pass
+
+
+def test_filter_removed():
+    df = pd.DataFrame({"test_0": ["[removed]", "[deleted]", "valid"], "test_1": [1, 2, 3]})
+    result = filter_removed.run(df, cols_to_check_if_removed=["test_0"]).reset_index(drop=True)
+    expected = pd.DataFrame({"test_0": ["valid"], "test_1": [3]}).reset_index(drop=True)
+    assert_frame_equal(result, expected, check_index_type=False)
+
+
+def test_add_temporal_informations():
+    df = pd.DataFrame({"created_utc": [1622057400]})
+    result = add_temporal_informations.run(df).reset_index(drop=True)
+    expected = pd.DataFrame(
+        {'created_utc': 1622057400, 'date': Timestamp('2021-05-26 21:30:00+0200', tz='Europe/Berlin'),
+         'date_day': Period('2021-05-26', 'D'), 'start': Timestamp('2021-05-26 21:00:00'),
+         'start_timestamp': 1622062800, 'end': Timestamp('2021-05-26 22:00:00'), 'end_timestamp': 1622066400},
+        index=[0]).reset_index(drop=True)
+    assert_frame_equal(result, expected)
+
+
+def test_filter_authors():
+    df = pd.DataFrame({"author": ["test_1", "test_2", "test_3", "blacklisted_author",
+                                  "author_with_too_many_subm", "author_with_too_many_subm",
+                                  "author_with_too_many_subm"],
+                       "num_comments": [10, 5, 0, 9, 66, 55, 44],
+                       "date_day": [Period('2021-05-26', 'D')] * 7})
+
+    # Test handling when filter_too_frequent_authors is False
+    assert_frame_equal(filter_authors.run(df, False, [], 0), df.copy())
+
+    # Test blacklist - tests whether blacklisted authors are filtered, therefore, set max_submissions_per_author_per_day
+    # higher than the highest amount of submissions per author and add an dummy author to the blacklist
+    result = filter_authors.run(df, True, author_blacklist=["blacklisted_author"],
+                                max_submissions_per_author_per_day=5)
+    expected = df[df["author"] != "blacklisted_author"]  # Filter the author that is blacklisted
+    assert_frame_equal(result, expected, check_like=True)  # check_list = True since order might differ (not relevant)
+
+    # Test max_submissions
+    #   expected behaviour: the submissions of authors, where the number of total submissions is higher than
+    #   max_submissions_per_author_per_day should be reduced to max_submissions_per_author_per_day (e.g. only those
+    #   submissions that fit into max_submissions_per_author_per_day and have the highest number of comments should be
+    #   kept
+    result = filter_authors.run(df, True, author_blacklist=[],
+                                max_submissions_per_author_per_day=2)
+    # Submission with lowest n_comments from author_with_too_many_subm should be filtered
+    expected = df[df["num_comments"] != 44]
+    assert_frame_equal(result, expected, check_like=True)
+
+
+def test_delete_non_alphanumeric():
+    df = pd.DataFrame({"text": ["text", "text?", "text!", "text12345", "%/§%(&§%(alphanumeric", ":):D"],
+                       "ignore": [0, 1, 2, 3, 4, 5]})
+    result = delete_non_alphanumeric.run(df, cols_to_be_cleaned=["text"])
+    expected = pd.DataFrame(
+        [{'text': 'text', 'ignore': 0}, {'text': 'text?', 'ignore': 1}, {'text': 'text!', 'ignore': 2},
+         {'text': 'text12345', 'ignore': 3}, {'text': '((alphanumeric', 'ignore': 4}, {'text': ':):D', 'ignore': 5}])
+    assert_frame_equal(result, expected)
+
+
+def test_extract_ticker():
+    valid_ticker = ["GME", "TSLA", "AAPL"]
+
+    txt = "GME TSLA AAPL"
+    # Convert expected to list(set()) since order might be changed in extract_ticker
+    assert extract_ticker(txt, valid_ticker=valid_ticker, ticker_blacklist=[]) == ["GME", "TSLA", "AAPL"]
+
+    # TODO Recognize all ticker in this case
+    txt = "GME, TSLA, AAPL"
+    assert extract_ticker(txt, valid_ticker=valid_ticker, ticker_blacklist=[]) == ["AAPL"]
+
+    txt = "GME&TSLA&AAPL"
+    assert extract_ticker(txt, valid_ticker=valid_ticker, ticker_blacklist=[]) == None
+
+    txt = "GME & TSLA & AAPL"
+    assert extract_ticker(txt, valid_ticker=valid_ticker, ticker_blacklist=[]) == ["GME", "TSLA", "AAPL"]
+
+    # Assert that multiple entries are reduced to one
+    txt = "GME GME GME"
+    assert extract_ticker(txt, valid_ticker=valid_ticker, ticker_blacklist=[]) == ['GME']
+
+    # Check blacklist
+    txt = "GME TSLA AAPL BLK"
+    assert extract_ticker(txt, valid_ticker=valid_ticker, ticker_blacklist=["BLK"]) == ["GME", "TSLA", "AAPL"]
+
+
+def test_get_submission_ticker():
+    valid_ticker = ["GME", "AAPL", "TSLA", "BLK"]
+
+    # Case 1: Find valid ticker in title only
+    df = pd.DataFrame({"title": ["GME AAPL", "TSLA", "INVALID"], "selftext": ["GME", "INVALID", "GME"]})
+    result = get_submission_ticker.run(df, valid_ticker=valid_ticker, ticker_blacklist=[], search_ticker_in_body=False)
+    expected = pd.DataFrame({"title": ["GME AAPL", "TSLA", "INVALID"], "selftext": ["GME", "INVALID", "GME"],
+                             "title_ticker": [["GME", "AAPL"], ["TSLA"], None], "body_ticker": [None, None, None]})
+    assert_frame_equal(result, expected)
+
+    # Case 2: Ignore blacklisted (but valid) ticker
+    df = pd.DataFrame({"title": ["GME AAPL", "TSLA", "INVALID", "BLK"], "selftext": ["GME", "INVALID", "GME", "GME"]})
+    ticker_blacklist = ["BLK"]
+    result = get_submission_ticker.run(df, valid_ticker=valid_ticker, ticker_blacklist=ticker_blacklist,
+                                       search_ticker_in_body=False)
+    expected = pd.DataFrame({"title": ["GME AAPL", "TSLA", "INVALID", "BLK"],
+                             "selftext": ["GME", "INVALID", "GME", "GME"],
+                             "title_ticker": [["GME", "AAPL"], ["TSLA"], None, None],
+                             "body_ticker": [None, None, None, None]})
+    assert_frame_equal(result, expected)
+
+    # Case 3: Find valid ticker in body if no valid ticker was found in title
+    df = pd.DataFrame({"title": ["GME AAPL", "TSLA", "INVALID"], "selftext": ["GME", "INVALID", "GME"]})
+    result = get_submission_ticker.run(df, valid_ticker=valid_ticker, ticker_blacklist=[], search_ticker_in_body=True)
+    expected = pd.DataFrame({"title": ["GME AAPL", "TSLA", "INVALID"], "selftext": ["GME", "INVALID", "GME"],
+                             "title_ticker": [["GME", "AAPL"], ["TSLA"], None], "body_ticker": [None, None, ["GME"]]})
+    assert_frame_equal(result, expected)
+
+
+def test_filter_submissions_without_ticker():
+    df = pd.DataFrame({"title_ticker": [["GME"], None, None], "body_ticker": [None, None, ["GME"]]})
+    result = filter_submissions_without_ticker.run(df)
+    expected = pd.DataFrame({"title_ticker": [["GME"], None], "body_ticker": [None, ["GME"]]})
+    assert_frame_equal(result.reset_index(drop=True), expected.reset_index(drop=True))
+
+
+def test_merge_ticker_to_a_single_column():
+    df = pd.DataFrame({"title_ticker": [["GME"], None, None], "body_ticker": [None, ["TSLA"], ["GME"]]})
+    result = merge_ticker_to_a_single_column.run(df)
+    expected = pd.DataFrame({"ticker": [["GME"], ["TSLA"], ["GME"]]})
+    assert_frame_equal(result, expected)
+
+
+def test_analyze_sentiment():
+    df = pd.DataFrame({"title": ["VADER is smart, handsome, and funny.",
+                                 "At least it isn't a horrible book.",
+                                 "The book was good."]})
+    result = analyze_sentiment.run(df)
+    expected = pd.DataFrame(
+        [{'title': 'VADER is smart, handsome, and funny.', 'pos': 0.746, 'neu': 0.254, 'neg': 0.0, 'compound': 0.8316},
+         {'title': "At least it isn't a horrible book.", 'pos': 0.363, 'neu': 0.637, 'neg': 0.0, 'compound': 0.431},
+         {'title': 'The book was good.', 'pos': 0.492, 'neu': 0.508, 'neg': 0.0, 'compound': 0.4404}], index=[0, 1, 2])
+    assert_frame_equal(result, expected)
+
+
+def test_flatten_ticker_scores():
+    df = pd.DataFrame({"ticker": [["GME", "TSLA"], ["TSLA"], ["GME"]], "id": [0, 1, 2]})
+    result = flatten_ticker_scores.run(df)
+    expected = pd.DataFrame([{'ticker': 'GME', 'id': 0}, {'ticker': 'TSLA', 'id': 0}, {'ticker': 'TSLA', 'id': 1},
+                             {'ticker': 'GME', 'id': 2}])
+    # dtype changes since we move from list to str for column "ticker"
+    assert_frame_equal(result.reset_index(drop=True), expected.reset_index(drop=True), check_dtype=False)
+
+
+def test_retrieve_timespans():
+    df = pd.DataFrame({"submissions": ["subm_1", "subm_2"],
+                       "start": [Timestamp('2021-05-26 21:00:00'),
+                                 Timestamp('2021-05-26 22:00:00')],
+                       "end": [Timestamp('2021-05-26 22:00:00'),
+                               Timestamp('2021-05-26 23:00:00')]})
+    result = retrieve_timespans.run(df, relevant_timespan_cols=["submissions"])
+
+    # Expected: each "submission" should be in its own Timespan object since they are not within the same
+    # start/end period
+    ts_1_df = pd.DataFrame({"submissions": ["subm_1"]})
+    ts_1 = Timespan(df=ts_1_df, start=Timestamp('2021-05-26 21:00:00'), end=Timestamp('2021-05-26 22:00:00'))
+
+    assert result[0] == ts_1
+
+    ts_2_df = pd.DataFrame({"submissions": ["subm_2"]})
+    ts_2 = Timespan(df=ts_2_df, start=Timestamp('2021-05-26 22:00:00'), end=Timestamp('2021-05-26 23:00:00'))
+    assert result[1] == ts_2
+
+
+def test_aggregate_submissions_per_timespan():
+    df = pd.DataFrame({"ticker": ["GME", "GME", "GME", "TSLA", "TSLA", "EMA"], "pos": [0.5, 0.5, 0.5, 7.0, 7.0, 10.0],
+                       "will_be_dropped": ["yes", "yes", "yes", "yes", "yes", "yes"]})
+    ts = Timespan(df=df, start=None, end=None)
+    result = aggregate_submissions_per_timespan.run(ts)
+    expected = pd.DataFrame([{'ticker': 'EMA', 'pos': 10.0, 'num_posts': 1},
+                             {'ticker': 'GME', 'pos': 1.5, 'num_posts': 3},
+                             {'ticker': 'TSLA', 'pos': 14.0, 'num_posts': 2}])
+    assert_frame_equal(result.df, expected)
+
+
+def test_summarize_timespans():
+    ts_1_df = pd.DataFrame({"submissions": ["subm_1"]})
+    ts_1 = Timespan(df=ts_1_df, start=Timestamp('2021-05-26 21:00:00'), end=Timestamp('2021-05-26 22:00:00'))
+
+    ts_2_df = pd.DataFrame({"submissions": ["subm_2"]})
+    ts_2 = Timespan(df=ts_2_df, start=Timestamp('2021-05-26 22:00:00'), end=Timestamp('2021-05-26 23:00:00'))
+
+    result = summarize_timespans.run([ts_1, ts_2])
+    expected = pd.DataFrame([
+        {'submissions': 'subm_1', 'start': Timestamp('2021-05-26 21:00:00'), 'end': Timestamp('2021-05-26 22:00:00')},
+        {'submissions': 'subm_2', 'start': Timestamp('2021-05-26 22:00:00'), 'end': Timestamp('2021-05-26 23:00:00')}])
+    assert_frame_equal(result, expected)
