ad89b39844b4dc43326b21f318d140ac8fc56030
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-06-10 19:11:39 +0200

    Rewrote sequence generation

---
+++ preprocessing/logic/sequences.py
@@ -0,0 +1,92 @@
+import pandas as pd
+
+
+class Sequence:
+
+    def __init__(self, df: pd.DataFrame, sequence_len: int, include_available_days_only: bool):
+        """
+        Generates sequences from a given dataframe.
+
+        Args:
+            df: Input dataframe
+            sequence_len: Length of the desired sequence
+            include_available_days_only: Whether to filter sequences which were not available for trading
+        """
+        self.df = df
+        self.sequence_len = sequence_len
+        self.include_available_days_only = include_available_days_only
+
+        self._sequences = []
+
+    def slice_sequences(self) -> list:
+        """
+        Slices several parts out of a df - based on the sequence len.
+
+        Example (in non-dataframe format) with sequence_len = 3:
+        1, 2, 3, 4, 5, 6, 7
+        -> [[1,2,3], [2,3,4], [3,4,5], [4,5,6], [5,6,7]]
+        """
+        for i in range(len(self.df) - self.sequence_len + 1):
+            sliced = self.df[i: i + self.sequence_len]
+            self._sequences.append(sliced)
+        return self._sequences
+
+    def filter_availability(self) -> list:
+        """
+        Filters a sliced df based on whether the last element from this df is available or not. We take the last element
+        as a reference since this is the "current" element in this case - the elements prior to the last element are
+        "past" elements. For instance, if we look at wednesday and have the data from monday and tuesday - it doesn't
+        matter whether it was possible to trade on monday or tuesday, all that matters is that we can trade on
+        wednesday.
+        """
+        if not self.include_available_days_only:
+            return self._sequences
+
+        valid_sequences = []
+        for sequence in self._sequences:
+            availability_of_last_sequence_element = sequence["available"].iloc[len(sequence) - 1]
+
+            if availability_of_last_sequence_element == True:
+                valid_sequences.append(sequence)
+
+        self._sequences = valid_sequences
+        return self._sequences
+
+    def make_sequence(self):
+        """
+        Wrapper to run through all steps to create a sequence
+        """
+        raise NotImplementedError
+
+    def to_list(self):
+        return self._sequences
+
+
+class FlatSequence(Sequence):
+
+    def flatten(self):
+        """
+        Flattens a df. Example: If we have 10 rows and 5 columns we will have 1 row and 50 columns afterwards. Each
+        value that gets transformed into a new column will have a prefix with an integer indicating the index in the
+        original df it was located at.
+        """
+        # TODO Might be faster to use numpy reshape here - but we would have to deal with the column names then
+        new_sequences = []
+        for sequence in self._sequences:
+            new_sequences.append(sequence.unstack().to_frame().sort_index(level=1).T)
+        self._sequences = new_sequences
+        return self._sequences
+
+    def make_sequence(self):
+        self.slice_sequences()
+        self.filter_availability()
+        self.flatten()
+        return self._sequences
+
+
+class ArraySequence(Sequence):
+
+    def make_sequence(self):
+        self.slice_sequences()
+        self.filter_availability()
+        return self._sequences
---
+++ tests/test_preprocessing/test_logic/test_sequences.py
@@ -0,0 +1,119 @@
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from preprocessing.logic.sequences import FlatSequence, ArraySequence, Sequence
+
+
+def test_sequence_without_availability():
+    df = pd.DataFrame({"dummy": [1, 2, 3, 4, 5, 6, 7]})
+
+    seq = Sequence(df, sequence_len=3, include_available_days_only=False)
+
+    result = seq.slice_sequences()
+    expected = [
+        pd.DataFrame({"dummy": [1, 2, 3]}),
+        pd.DataFrame({"dummy": [2, 3, 4]}),
+        pd.DataFrame({"dummy": [3, 4, 5]}),
+        pd.DataFrame({"dummy": [4, 5, 6]}),
+        pd.DataFrame({"dummy": [5, 6, 7]}),
+    ]
+
+    for r, e in zip(result, expected):
+        assert_frame_equal(r.reset_index(drop=True), e)
+
+    result = seq.filter_availability()
+    for r, e in zip(result, expected):
+        assert_frame_equal(r.reset_index(drop=True), e)
+
+
+def test_sequence_longer_sequence_len():
+    df = pd.DataFrame({"dummy": [1, 2, 3, 4, 5, 6, 7]})
+
+    seq = Sequence(df, sequence_len=4, include_available_days_only=False)
+
+    result = seq.slice_sequences()
+    expected = [
+        pd.DataFrame({"dummy": [1, 2, 3, 4]}),
+        pd.DataFrame({"dummy": [2, 3, 4, 5]}),
+        pd.DataFrame({"dummy": [3, 4, 5, 6]}),
+        pd.DataFrame({"dummy": [4, 5, 6, 7]}),
+        pd.DataFrame({"dummy": [5, 6, 7, 8]}),
+    ]
+
+    for r, e in zip(result, expected):
+        assert_frame_equal(r.reset_index(drop=True), e)
+
+    result = seq.filter_availability()
+    for r, e in zip(result, expected):
+        assert_frame_equal(r.reset_index(drop=True), e)
+
+
+def test_sequence_with_availability():
+    df = pd.DataFrame({"dummy": [1, 2, 3, 4, 5, 6, 7], "available": [False, False, True, True, True, True, True]})
+    seq = Sequence(df, sequence_len=3, include_available_days_only=True)
+    seq.slice_sequences()
+    result = seq.filter_availability()
+
+    expected = [
+        pd.DataFrame({"dummy": [1, 2, 3], "available": [False, False, True]}),
+        pd.DataFrame({"dummy": [2, 3, 4], "available": [False, True, True]}),
+        pd.DataFrame({"dummy": [3, 4, 5], "available": [True, True, True]}),
+        pd.DataFrame({"dummy": [4, 5, 6], "available": [True, True, True]}),
+        pd.DataFrame({"dummy": [5, 6, 7], "available": [True, True, True]}),
+    ]
+
+    for r, e in zip(result, expected):
+        assert_frame_equal(r.reset_index(drop=True), e)
+
+    # More not available dummies
+    df = pd.DataFrame({"dummy": [1, 2, 3, 4, 5, 6, 7], "available": [False, False, False, True, True, True, True]})
+    seq = Sequence(df, sequence_len=3, include_available_days_only=True)
+    seq.slice_sequences()
+    result = seq.filter_availability()
+
+    expected = [
+        pd.DataFrame({"dummy": [2, 3, 4], "available": [False, False, True]}),
+        pd.DataFrame({"dummy": [3, 4, 5], "available": [False, True, True]}),
+        pd.DataFrame({"dummy": [4, 5, 6], "available": [True, True, True]}),
+        pd.DataFrame({"dummy": [5, 6, 7], "available": [True, True, True]}),
+    ]
+
+    for r, e in zip(result, expected):
+        assert_frame_equal(r.reset_index(drop=True), e)
+
+
+def test_flat_sequence():
+    df = pd.DataFrame({"dummy": [1, 2, 3, 4, 5, 6, 7]})
+
+    fs = FlatSequence(df=df, sequence_len=3, include_available_days_only=False)
+    result = fs.make_sequence()
+
+    expected = [
+        pd.DataFrame({"dummy/0": [1], "dummy/1": [2], "dummy/2": [3]}),
+        pd.DataFrame({"dummy/1": [2], "dummy/2": [3], "dummy/3": [4]}),
+        pd.DataFrame({"dummy/2": [3], "dummy/3": [4], "dummy/4": [5]}),
+        pd.DataFrame({"dummy/3": [4], "dummy/4": [5], "dummy/5": [6]}),
+        pd.DataFrame({"dummy/4": [5], "dummy/5": [6], "dummy/6": [7]}),
+    ]
+
+    for r, e in zip(result, expected):
+        r.columns = e.columns  # r uses multi-level index, e doesn't (doesn't matter for the comparison tho)
+        assert_frame_equal(r, e, check_column_type=False)
+
+
+def test_arr_sequence():
+    df = pd.DataFrame({"dummy": [1, 2, 3, 4, 5, 6, 7]})
+
+    seq = ArraySequence(df=df, sequence_len=3, include_available_days_only=False)
+    result = seq.make_sequence()
+
+    expected = [
+        pd.DataFrame({"dummy": [1, 2, 3]}),
+        pd.DataFrame({"dummy": [2, 3, 4]}),
+        pd.DataFrame({"dummy": [3, 4, 5]}),
+        pd.DataFrame({"dummy": [4, 5, 6]}),
+        pd.DataFrame({"dummy": [5, 6, 7]}),
+    ]
+
+    for r, e in zip(result, expected):
+        assert_frame_equal(r.reset_index(drop=True), e)
