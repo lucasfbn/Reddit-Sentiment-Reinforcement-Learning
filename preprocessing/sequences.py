82976cbf6f73b44549b957782bbbb9bbb0594444
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-06-16 14:36:20 +0200

    Sequences are now represented by sequence objects which have additional attributes like: tradeable, available, price for easier use later on

--- preprocessing/sequences.py
+++ preprocessing/sequences.py
@@ -1,6 +1,18 @@
 import pandas as pd
 
 
+class Sequence:
+
+    def __init__(self, price, available, tradeable, df):
+        self.df = df
+        self.tradeable = tradeable
+        self.available = available
+        self.price = price
+
+    def __len__(self):
+        return len(self.df)
+
+
 class SequenceGenerator:
 
     def __init__(self, df: pd.DataFrame, sequence_len: int, include_available_days_only: bool, last_column: str = None,
@@ -23,6 +35,7 @@ class SequenceGenerator:
         self.last_column = last_column
         self.exclude_cols_from_sequence = exclude_cols_from_sequence
 
+        self._sliced = []
         self._sequences = []
 
     def slice_sequences(self) -> list:
@@ -35,46 +48,71 @@ class SequenceGenerator:
         """
         for i in range(len(self.df) - self.sequence_len + 1):
             sliced = self.df[i: i + self.sequence_len]
-            self._sequences.append(sliced)
+            self._sliced.append(sliced)
+        return self._sliced
+
+    @staticmethod
+    def _add_availability(df):
+        """
+        We take the last element as a reference since this is the "current" element in this case - the elements prior
+        to the last element are "past" elements. For instance, if we look at wednesday and have the data from monday
+        and tuesday - it doesn't matter whether it was possible to trade on monday or tuesday, all that matters is that
+        we can trade on wednesday.
+        """
+        return bool(df["available"].iloc[len(df) - 1])
+
+    @staticmethod
+    def _add_price(df):
+        """
+        See @_add_availability
+        """
+        return float(df["price"].iloc[len(df) - 1])
+
+    @staticmethod
+    def _add_tradeable(df):
+        """
+        See @_add_availability
+        """
+        return bool(df["tradeable"].iloc[len(df) - 1])
+
+    def sliced_to_sequence_obj(self):
+        """
+        Transforms each slice into a Sequence object.
+        """
+
+        for slice in self._sliced:
+            self._sequences.append(
+                Sequence(
+                    available=self._add_availability(slice),
+                    tradeable=self._add_tradeable(slice),
+                    price=self._add_price(slice),
+                    df=slice
+                ))
         return self._sequences
 
     def filter_availability(self) -> list:
         """
-        Filters a sliced df based on whether the last element from this df is available or not. We take the last element
-        as a reference since this is the "current" element in this case - the elements prior to the last element are
-        "past" elements. For instance, if we look at wednesday and have the data from monday and tuesday - it doesn't
-        matter whether it was possible to trade on monday or tuesday, all that matters is that we can trade on
-        wednesday.
+        Filters a sliced df based on whether the last element from this df is available or not.
         """
         if not self.include_available_days_only:
             return self._sequences
 
-        valid_sequences = []
-        for sequence in self._sequences:
-            availability_of_last_sequence_element = sequence["available"].iloc[len(sequence) - 1]
-
-            if availability_of_last_sequence_element == True:
-                valid_sequences.append(sequence)
-
-        self._sequences = valid_sequences
+        self._sequences = [seq for seq in self._sequences if seq.available is True]
         return self._sequences
 
     def handle_column_order(self):
         """
         Reorders the columns of a sequence. The last_column will be the last column. (captain obvious)
         """
-        if self.last_column is None or not self._sequences:  # If sequence is empty
+        if self.last_column is None or not self._sequences:  # If sequences is empty
             return self._sequences
 
-        cols = self._sequences[0].columns.tolist()
+        cols = self._sequences[0].df.columns.tolist()
         cols.remove(self.last_column)
         cols += [self.last_column]
 
-        new_sequences = []
         for seq in self._sequences:
-            seq = seq[cols]
-            new_sequences.append(seq)
-        self._sequences = new_sequences
+            seq.df = seq.df[cols]
         return self._sequences
 
     def exclude_columns(self):
@@ -84,14 +122,13 @@ class SequenceGenerator:
         if not self.exclude_cols_from_sequence:
             return self._sequences
 
-        new_sequences = []
         for seq in self._sequences:
-            new_sequences.append(seq.drop(columns=self.exclude_cols_from_sequence))
-        self._sequences = new_sequences
+            seq.df = seq.df.drop(columns=self.exclude_cols_from_sequence)
         return self._sequences
 
     def _make_sequence(self):
         self.slice_sequences()
+        self.sliced_to_sequence_obj()
         self.filter_availability()
         self.handle_column_order()
         self.exclude_columns()
@@ -115,10 +152,8 @@ class FlatSequenceGenerator(SequenceGenerator):
         original df it was located at.
         """
         # TODO Might be faster to use numpy reshape here - but we would have to deal with the column names then
-        new_sequences = []
         for sequence in self._sequences:
-            new_sequences.append(sequence.unstack().to_frame().T)
-        self._sequences = new_sequences
+            sequence.df = sequence.df.unstack().to_frame().T
         return self._sequences
 
     def make_sequence(self):
