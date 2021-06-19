1f7d8ba6aa326f1e9a6dc73fcfcd50d89ec87435
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-06-19 20:42:05 +0200

    Removed metadata from ticker

--- preprocessing/ticker.py
+++ preprocessing/ticker.py
@@ -3,7 +3,6 @@ class Ticker:
     def __init__(self, name, df):
         self.df = df
         self.name = name
-        self.metadata = None
         self.exclude = False
 
         self.flat_sequence = None
