af58b6c312d2caacd05379c6ba49baefdc9c11ff
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-06-30 22:14:02 +0200

    Implemented cache

---
+++ tests/test_preprocessing/test_price_data/test_cache.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from pandas import Period
+from pandas.testing import assert_frame_equal
+
+from preprocessing.price_data.cache import Cache
+
+
+def test_first_append():
+    c = Cache(db_path=":memory:")
+    df = pd.DataFrame({'ticker': ["AAPL", "TSLA", "GOE"], "Close": [1, 2, 3], "date_day": [Period('2021-05-10', 'D'),
+                                                                                           Period('2021-05-11', 'D'),
+                                                                                           Period('2021-05-12', 'D')]})
+    c.append(df)
+    result = c.get_all()
+    print(result.dtypes)
+    print(df.dtypes)
+
+    assert_frame_equal(df, result)
+
+
+def test_several_appends():
+    c = Cache(db_path=":memory:")
+    df = pd.DataFrame({'ticker': ["AAPL", "TSLA", "GOE"], "Close": [1, 2, 3], "date_day": [Period('2021-05-10', 'D'),
+                                                                                           Period('2021-05-11', 'D'),
+                                                                                           Period('2021-05-12', 'D')]})
+    c.append(df)
+    c.append(df)
+    c.append(df)
+    result = c.get_all()
+
+    expected = pd.DataFrame(
+        {'ticker': ["AAPL", "TSLA", "GOE"] * 3, "Close": [1, 2, 3] * 3, "date_day": [Period('2021-05-10', 'D'),
+                                                                                     Period('2021-05-11', 'D'),
+                                                                                     Period('2021-05-12', 'D')] * 3})
+
+    assert_frame_equal(expected, result)
+
+
+def test_drop_duplicates():
+    c = Cache(db_path=":memory:")
+    df = pd.DataFrame({'ticker': ["AAPL", "TSLA", "GOE"], "Close": [1, 2, 3], "date_day": [Period('2021-05-10', 'D'),
+                                                                                           Period('2021-05-11', 'D'),
+                                                                                           Period('2021-05-12', 'D')]})
+    c.append(df)
+    c.append(df)
+    c.append(df)
+    result = c.get_all()
+
+    expected = pd.DataFrame(
+        {'ticker': ["AAPL", "TSLA", "GOE"] * 3, "Close": [1, 2, 3] * 3, "date_day": [Period('2021-05-10', 'D'),
+                                                                                     Period('2021-05-11', 'D'),
+                                                                                     Period('2021-05-12', 'D')] * 3})
+
+    assert_frame_equal(expected, result)
+
+    c.drop_duplicates()
+    result = c.get_all()
+    assert_frame_equal(df, result)
+
+
+def test_get_specific():
+    c = Cache(db_path=":memory:")
+    df = pd.DataFrame({'ticker': ["AAPL", "TSLA", "GOE"], "Close": [1, 2, 3], "date_day": [Period('2021-05-10', 'D'),
+                                                                                           Period('2021-05-11', 'D'),
+                                                                                           Period('2021-05-12', 'D')]})
+    c.append(df)
+    result = c.get("TSLA")
+
+    expected = pd.DataFrame({'ticker': ["TSLA"], "Close": [2], "date_day": [Period('2021-05-11', 'D')]})
+
+    assert_frame_equal(expected, result)
