2d9664103973c6ac9bd641814f2826d72a78b902
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-07-20 19:15:41 +0200

    Added tests for functions that return several arguments

--- tests/test_utils/test_pipeline_utils.py
+++ tests/test_utils/test_pipeline_utils.py
@@ -11,6 +11,12 @@ def loops(x, constant):
     return x + constant
 
 
+@task
+def loops_several_arguments(x, constant):
+    time.sleep(0.1)
+    return x + constant, x * constant
+
+
 def test_task_exec():
     assert loops(1, 2).run() == 3
 
@@ -27,3 +33,17 @@ def test_task_parallel_exec():
     expected = [3, 4, 5, 6]
 
     assert par_map(loops, input, constant=2).run() == expected
+
+
+def test_multiple_return_values_seq():
+    input = list(range(1, 5))
+    x, y = map(list, zip(*seq_map(loops_several_arguments, input, constant=2).run()))
+
+    assert x == [3, 4, 5, 6] and y == [2, 4, 6, 8]
+
+
+def test_multiple_return_values_par():
+    input = list(range(1, 5))
+    x, y = map(list, zip(*seq_map(loops_several_arguments, input, constant=2).run()))
+
+    assert x == [3, 4, 5, 6] and y == [2, 4, 6, 8]
