a938a30f11c29fb0521989b40aa8acd47a8b6ea1
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-07-20 20:14:33 +0200

    Migrated preprocessing to own pipeline framework

--- preprocessing/pipeline.py
+++ preprocessing/pipeline.py
@@ -1,110 +1,102 @@
-import os
-
-os.environ['PREFECT__LOGGING__LEVEL'] = "INFO"
-
 import mlflow
-import prefect
-from prefect import Flow, Parameter, unmapped
-from prefect.engine.state import Success
-from prefect.executors import LocalExecutor, LocalDaskExecutor
 
 import paths
 from preprocessing.tasks import *
-from utils.util_tasks import mlflow_log_file, unpack_union_mapping, reduce_list
-from utils.mlflow_api import load_file
-from preprocessing.ticker import Ticker
+from utils.mlflow_api import load_file, log_file
+from utils.pipeline_utils import initialize, par_map, seq_map
 
 mlflow.set_tracking_uri(paths.mlflow_path)
 mlflow.set_experiment("Tests")
 
-with Flow("preprocessing") as flow:
-    input_df = Parameter("input_df")
-    start_hour = Parameter("start_hour", default=21)
-    start_min = Parameter("start_min", default=0)
+initialize()
 
-    sentiment_data_columns = Parameter("sentiment_data_columns",
-                                       default=["num_comments", "score", "pos", "neu", "neg", "compound",
-                                                "num_posts"])
-    price_data_columns = Parameter("price_data_columns", default=["Open", "High", "Low", "Close", "Volume"])
-    price_column = Parameter("price_column", default="Close")
+params = {
+    "input_df": None,
+    "start_hour": 21,
+    "start_min": 0,
+    "sentiment_data_columns": ["num_comments", "score", "pos", "neu", "neg", "compound",
+                               "num_posts"],
+    "price_data_columns": ["Open", "High", "Low", "Close", "Volume"],
+    "price_column": "Close",
+    "drop_unscaled_cols": True,
+    "ticker_min_len": 6,
+    "price_data_start_offset": 10,
+    "enable_live_behaviour": False,
+    "include_available_days_only": True,
+    "sequence_length": 7,
+    "columns_to_be_excluded_from_sequences": ["available", "tradeable", main_date_col,
+                                              "sentiment_data_available", "price_raw", "Open_scaled",
+                                              "High_scaled", "Low_scaled", "Volume_scaled"],
+    "sequence_to_be_generated": "arr",
+    "main_date_col_param": main_date_col,
+    "start_end_columns": ["start", "end"],
+    "adj_close_column_plus_merged": ["Adj_Close", "_merge"]
+}
 
-    drop_unscaled_cols = Parameter("drop_unscaled_cols", default=True)
-    ticker_min_len = Parameter("ticker_min_len", default=6)
-    price_data_start_offset = Parameter("price_data_start_offset", default=10)
-    enable_live_behaviour = Parameter("enable_live_behaviour", default=False)
-    include_available_days_only = Parameter("include_available_days_only", default=True)
-    sequence_length = Parameter("sequence_length", default=7)
-    columns_to_be_excluded_from_sequences = Parameter("columns_to_be_excluded_from_sequences",
-                                                      default=["available", "tradeable", main_date_col,
-                                                               "sentiment_data_available", "price_raw", "Open_scaled",
-                                                               "High_scaled", "Low_scaled", "Volume_scaled"])
-    sequence_to_be_generated = Parameter("sequence_to_be_generated", default="arr")
-    main_date_col_param = Parameter("main_date_col_param", main_date_col)
 
-    df = add_time(input_df)
-    df = shift_time(df, start_hour, start_min)
-    df = drop_columns(df, Parameter("start_end_columns", default=["start", "end"]))
+def pipeline():
+    df = add_time(params["input_df"]).run()
+    df = shift_time(df, params["start_hour"], params["start_min"]).run()
+    df = drop_columns(df, params["start_end_columns"]).run()
 
-    df, sentiment_data_columns = scale_sentiment_data_daywise(df, sentiment_data_columns, drop_unscaled_cols)
-    ticker = grp_by_ticker(df)
-    ticker = aggregate_daywise.map(ticker)
-    ticker = drop_ticker_with_too_few_data(ticker, ticker_min_len)
-    ticker = sort_ticker_df_chronologically.map(ticker, unmapped(main_date_col_param))
-    ticker = mark_trainable_days.map(ticker, unmapped(ticker_min_len))
-    ticker = add_price_data.map(ticker, unmapped(price_data_start_offset), unmapped(enable_live_behaviour))
-    _ = clean_price_data_cache(ticker)
-    ticker = remove_excluded_ticker(ticker)
-    ticker = mark_sentiment_data_available_days.map(ticker, unmapped(sentiment_data_columns))
-    ticker = drop_ticker_df_columns.map(ticker, unmapped(Parameter("adj_close_column_plus_merged",
-                                                                   default=["Adj_Close", "_merge"])))
-    ticker = sort_ticker_df_chronologically.map(ticker, unmapped(main_date_col_param))
-    ticker = backfill_availability.map(ticker)
-    ticker = assign_price_col.map(ticker, unmapped(price_column))
-    ticker = drop_ticker_df_columns.map(ticker, unmapped(price_column))
-    price_data_columns = remove_old_price_col_from_price_data_columns(price_data_columns, price_column)
-    ticker = mark_tradeable_days.map(ticker)
-    ticker = forward_fill_price_data.map(ticker, unmapped(price_data_columns))
-    ticker = mark_ticker_where_all_prices_are_nan.map(ticker)
-    ticker = mark_ipo_ticker.map(ticker)
-    ticker = remove_excluded_ticker(ticker)
-    ticker = fill_missing_sentiment_data.map(ticker, unmapped(sentiment_data_columns))
-    ticker = add_metric_rel_price_change.map(ticker)
-    ticker = drop_ticker_df_columns.map(ticker, unmapped(Parameter("date_cols", default=[main_date_col])))
-    _ = mlflow_log_file(ticker, "ticker.pkl")
-    _ = assert_no_nan.map(ticker)
-    ticker = copy_unscaled_price.map(ticker)
+    df, params["sentiment_data_columns"] = scale_sentiment_data_daywise(df, params["sentiment_data_columns"],
+                                                                        params["drop_unscaled_cols"]).run()
+    ticker = grp_by_ticker(df).run()
+    ticker = seq_map(aggregate_daywise, ticker).run()
+    ticker = seq_map(drop_ticker_with_too_few_data, ticker, ticker_min_len=params["ticker_min_len"]).run()
+    ticker = remove_excluded_ticker(ticker).run()
+    ticker = seq_map(sort_ticker_df_chronologically, ticker, by=params["main_date_col_param"]).run()
+    ticker = seq_map(mark_trainable_days, ticker, ticker_min_len=params["ticker_min_len"]).run()
+    ticker = par_map(add_price_data, ticker,  # TODO Put to par in final
+                     price_data_start_offset=params["price_data_start_offset"],
+                     enable_live_behaviour=params["enable_live_behaviour"]).run()
+    clean_price_data_cache().run()
+    ticker = remove_excluded_ticker(ticker).run()
+    ticker = seq_map(mark_sentiment_data_available_days, ticker,
+                     sentiment_data_columns=params["sentiment_data_columns"]).run()
+    ticker = seq_map(drop_ticker_df_columns, ticker, columns_to_be_dropped=params["adj_close_column_plus_merged"]).run()
+    ticker = seq_map(sort_ticker_df_chronologically, ticker, by=params["main_date_col_param"]).run()
+    ticker = seq_map(backfill_availability, ticker).run()
+    ticker = seq_map(assign_price_col, ticker, price_col=params["price_column"]).run()
+    ticker = seq_map(drop_ticker_df_columns, ticker, columns_to_be_dropped=params["price_column"]).run()
+    params["price_data_columns"] = remove_old_price_col_from_price_data_columns(
+        price_data_columns=params["price_data_columns"],
+        price_column=params["price_column"]).run()
+    ticker = seq_map(mark_tradeable_days, ticker).run()
+    ticker = seq_map(forward_fill_price_data, ticker, price_data_cols=params["price_data_columns"]).run()
+    ticker = seq_map(mark_ticker_where_all_prices_are_nan, ticker).run()
+    ticker = seq_map(mark_ipo_ticker, ticker).run()
+    ticker = remove_excluded_ticker(ticker).run()
+    ticker = seq_map(fill_missing_sentiment_data, ticker, sentiment_data_columns=params["sentiment_data_columns"]).run()
+    ticker = seq_map(add_metric_rel_price_change, ticker).run()
+    _ = log_file(ticker, "ticker.pkl")
+    seq_map(assert_no_nan, ticker).run()
+    ticker = seq_map(copy_unscaled_price, ticker).run()
 
-    # Cannot unpack directly, therefore we need to unpack manually
-    temp_result = scale_price_data.map(ticker, unmapped(price_data_columns), unmapped(drop_unscaled_cols))
-    ticker, price_data_columns = unpack_union_mapping(temp_result)
-    price_data_columns = reduce_list(price_data_columns)
+    # map(list, ...) splits the list of tuples to two lists, see tests of pipeline_utils
+    ticker, price_data_columns = map(list, zip(*seq_map(scale_price_data, ticker,
+                                                        price_data_columns=params["price_data_columns"],
+                                                        drop_unscaled_cols=params["drop_unscaled_cols"]).run()))
+    params["price_data_columns"] = price_data_columns[0]
 
-    ticker = make_sequences.map(ticker, unmapped(sequence_length),
-                                unmapped(include_available_days_only),
-                                unmapped(columns_to_be_excluded_from_sequences),
-                                unmapped(Parameter("price_col", default="price_scaled")),
-                                unmapped(sequence_to_be_generated))
-    ticker = mark_empty_sequences.map(ticker)
-    ticker = remove_excluded_ticker(ticker)
+    ticker = par_map(make_sequences, ticker,
+                     sequence_length=params["sequence_length"],
+                     include_available_days_only=params["include_available_days_only"],
+                     columns_to_be_excluded_from_sequences=params["columns_to_be_excluded_from_sequences"],
+                     price_column="price_scaled",
+                     which=params["sequence_to_be_generated"]).run()
+    ticker = seq_map(mark_empty_sequences, ticker).run()
+    ticker = remove_excluded_ticker(ticker).run()
 
-    _ = mlflow_log_file(ticker, "ticker.pkl")
+    log_file(ticker, "ticker.pkl")
 
 
 def main(test_mode=False):
-    # flow.executor = LocalDaskExecutor(scheduler="processes", num_workers=8)
-    flow.executor = LocalExecutor()
-
-    if test_mode:
-        retrieve_task = flow.get_tasks("drop_ticker_df_columns")[0]
-        task_states = {retrieve_task: Success("test_state",
-                                              result=load_file("c47516069f3b40dfb1c88f5407659c96", "tickera.pkl")[:50])}
-        with mlflow.start_run():
-            flow.run(task_states=task_states)
-    else:
-        df = load_file("fde8b8735cbc4f2c950aa445b6682bf3", "report.csv")
-        # df = df.head(500)
-        with mlflow.start_run():
-            flow.run(dict(input_df=df))
+    df = load_file("fde8b8735cbc4f2c950aa445b6682bf3", "report.csv")
+    # df = df.head(500)
+    with mlflow.start_run():
+        params["input_df"] = df
+        pipeline()
 
 
 if __name__ == "__main__":
