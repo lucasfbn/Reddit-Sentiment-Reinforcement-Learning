e838709f8fb5e9069ee2cbecbe8d7bcd6215711c
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-08-25 22:20:30 +0200

    Simplified env, added transaction costs

--- rl/env.py
+++ rl/env.py
@@ -1,5 +1,4 @@
 from collections import deque
-from random import shuffle
 
 import mlflow
 import numpy as np
@@ -8,177 +7,148 @@ from tensorforce import Environment
 from utils.util_funcs import log
 
 
-class Env(Environment):
-    data = None
-    shuffle_sequences = True
-    exclude_non_tradeable_sequences = True
+class EnvCounter:
 
     def __init__(self):
+        self.full = 0
+        self.neg = 0
+        self.even = 0
+        self.pos = 0
+
+    def add_reward(self, reward):
+        self.full += reward
+
+        if reward == 0.0:
+            self.even += 1
+        elif reward > 0.0:
+            self.pos += 1
+        elif reward < 0.0:
+            self.neg += 1
+        else:
+            raise ValueError("Reward")
+
+    def log(self, step):
+        mlflow.log_metric("full run reward", self.full, step=step)
+        mlflow.log_metric("n_even_rewards", self.even, step=step)
+        mlflow.log_metric("n_pos_rewards", self.pos, step=step)
+        mlflow.log_metric("n_neg_rewards", self.neg, step=step)
+
+
+class Env(Environment):
+    TRANSACTION_COSTS_PERC = 0.01
+
+    def __init__(self, ticker):
         super().__init__()
 
-        self._episode_data = self.data
+        self.ticker = ticker
+        self.ticker_iter = 0
+        self.ticker_iter_max = len(self.ticker)
 
-        self._inventory = deque()
-        self._episode_reward = 0
+        self.curr_ticker = None
+        self.curr_env_counter = EnvCounter()
+        self.curr_sequences = None
+        self.curr_inventory = []
+        self.curr_sequence = None
 
-        self._current_ticker = None
-        self._current_sequences = None
-        self._states = None
-
-        self._episode_ended = False
-        self._state = None
-
-        # Counter
-        self._counter = 0
-        self._episode_counter = 0
-        self._neg_reward_counter = 0
-        self._even_reward_counter = 0
-        self._pos_reward_counter = 0
-
-        self._exclude_non_tradeable_sequences()
-
-    def _exclude_non_tradeable_sequences(self):
-        if Env.data is not None and self.exclude_non_tradeable_sequences:
-            new_ticker = []
-            for t in self._episode_data:
-                new_sequences = [seq for seq in t.sequences if seq.tradeable is True]
-                if len(new_sequences) != 0:
-                    t.sequences = new_sequences
-                    new_ticker.append(t)
-            self._episode_data = new_ticker
+        self._episode_end = False
+        self._episode_count = 0
 
     @staticmethod
-    def _shape_state(self, state):
-        raise NotImplemented
+    def shape_state(state):
+        raise NotImplementedError
 
-    def _calculate_margin(self, current_price):
+    def states(self):
+        raise NotImplementedError
+
+    def next_ticker(self):
+        if self.ticker_iter == self.ticker_iter_max:
+            self.ticker_iter = 0
+
+        r = self.ticker[self.ticker_iter]
+
+        self.ticker_iter += 1
+        self.curr_ticker = r
+        self.curr_sequences = deque(self.curr_ticker.sequences)
+
+    def next_sequence(self):
+        if len(self.curr_sequences) == 0:
+            self._episode_end = True
+            return None
+        else:
+            next_sequence = self.curr_sequences.popleft()
+            next_sequence = self.shape_state(next_sequence)
+            next_state = next_sequence.df
+            self.curr_sequence = next_sequence
+            return next_state
+
+    def calculate_margin(self, current_price):
         margin = 0
 
-        for buy_price in self._inventory:
+        for buy_price in self.curr_inventory:
             margin += current_price - buy_price
 
         return margin
 
-    def _handle_step_counter(self, reward):
-        self._episode_reward += reward
-
-        if reward == 0.0:
-            self._even_reward_counter += 1
-        elif reward > 0.0:
-            self._pos_reward_counter += 1
-        elif reward < 0.0:
-            self._neg_reward_counter += 1
-        else:
-            raise ValueError("Reward")
-
     def execute(self, actions):
-
         reward = 0
-        current_price = self._current_price()
+        price = self.curr_sequence.price
 
+        # Buy
         if actions == 1:
-            self._inventory.append(current_price)
-            log.debug(f"Buy: {current_price}, len inventory: {len(self._inventory)}")
+            self.curr_inventory.append(price)
 
+            reward -= price * self.TRANSACTION_COSTS_PERC
+
+            log.debug(f"BUY. Stock: {self.curr_ticker.name}. Relativ price: {self.curr_sequence.price}"
+                      f"Abs price: {self.curr_sequence.price_raw}")
+
+        # Sell
         elif actions == 2:
 
-            if len(self._inventory) > 0:
+            if len(self.curr_inventory) > 0:
 
-                margin = self._calculate_margin(current_price)
+                margin = self.calculate_margin(price)
                 reward += margin
-                log.debug(f"Added to reward: {margin}. Total reward: {reward}")
 
-                self._inventory = deque()
-                log.debug(f"Sell: {current_price}, Profit: {margin}")
+                self.curr_inventory = []
+
+                log.debug(f"SOLD: Stock: {self.curr_ticker.name}. Relativ price: {self.curr_sequence.price}"
+                          f"Abs price: {self.curr_sequence.price_raw}. Profit/Loss: {margin}")
             else:
                 log.debug(f"Attempted sell, but inventory is empty.")
 
-        if len(self._states) == 0:
-            self._episode_ended = True
-        else:
-            next_state = self._shape_state(self._states.popleft())
-            self._state = next_state
+        self.curr_env_counter.add_reward(reward)
 
-        self._handle_step_counter(reward)
-
-        return self._state.df, self._episode_ended, reward
-
-    def _handle_reset_counter(self):
-        mlflow.log_metric("full run reward", self._episode_reward, step=self._episode_counter)
-        mlflow.log_metric("n_even_rewards", self._even_reward_counter, step=self._episode_counter)
-        mlflow.log_metric("n_pos_rewards", self._pos_reward_counter, step=self._episode_counter)
-        mlflow.log_metric("n_neg_rewards", self._neg_reward_counter, step=self._episode_counter)
-
-        self._episode_reward = 0
-        self._even_reward_counter = 0
-        self._neg_reward_counter = 0
-        self._pos_reward_counter = 0
-
-    def _handle_counter(self):
-        self._counter += 1
-        self._episode_counter += 1
-
-        if self._counter >= len(self._episode_data):
-            self._counter = 0
-            self._handle_reset_counter()
-
-        mlflow.log_metric("counter", self._counter, step=self._episode_counter)
-
-    def _assign_new_ticker(self):
-        self._current_ticker = self._episode_data[self._counter]
-
-    def _assign_new_sequences(self):
-        self._current_sequences = self._current_ticker.sequences
-
-    def _assign_new_states(self):
-        if self.shuffle_sequences:
-            shuffle(self._current_sequences)
-        self._states = deque(self._current_sequences)
+        next_state = self.next_sequence()
+        return next_state, self._episode_end, reward
 
     def reset(self):
-        self._episode_ended = False
+        self.curr_env_counter.log(step=self._episode_count)
+        self.curr_env_counter = EnvCounter()
 
-        self._assign_new_ticker()
-        self._assign_new_sequences()
-        self._assign_new_states()
+        self._episode_end = False
+        self._episode_count += 1
 
-        self._handle_counter()
-        self._inventory = deque()
-
-        self._state = self._states.popleft()
-        self._state = self._shape_state(self._state)
-
-        return self._state.df
-
-    def max_episode_timesteps(self):
-
-        max_ = 0
-
-        for ticker in self.data:
-            if len(ticker.sequences) > max_:
-                max_ = len(ticker.sequences)
-
-        return max_
+        self.curr_inventory = []
+        self.next_ticker()
+        state = self.next_sequence()
+        return state
 
     def actions(self):
         return dict(type="int", num_values=3)
 
-    def _current_price(self):
-        return self._state.price
-
-    @staticmethod
-    def get_sequences(ticker):
-        return ticker.sequences
+    def max_episode_timesteps(self):
+        return max(len(tck) for tck in self.ticker)
 
 
 class EnvNN(Env):
 
     def states(self):
-        shape = self.data[0].sequences[0].flat.shape
+        shape = self.ticker[0].sequences[0].flat.shape
         return dict(type="float", shape=(shape[1],))
 
     @staticmethod
-    def _shape_state(state):
+    def shape_state(state):
         state.df = np.asarray(state.flat).astype("float32")
         state.df = state.df.reshape((state.df.shape[1],))
         return state
@@ -187,11 +157,11 @@ class EnvNN(Env):
 class EnvCNN(Env):
 
     def states(self):
-        shape = self.data[0].sequences[0].arr.shape
+        shape = self.ticker[0].sequences[0].arr.shape
         return dict(type="float", shape=(1, shape[0], shape[1]))
 
     @staticmethod
-    def _shape_state(state):
+    def shape_state(state):
         state.df = state.arr.values.reshape((1, state.arr.shape[0], state.arr.shape[1]))
         state.df = np.asarray(state.df).astype('float32')
         return state
