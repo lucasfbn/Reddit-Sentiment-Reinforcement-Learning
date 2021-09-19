409fd5c8fac2fda6514541d4487e586da91123df
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-09-19 20:56:27 +0200

    Implemented runner variant

---
+++ rl/attemps/sequential_single_stock_runner.py
@@ -0,0 +1,22 @@
+import mlflow
+
+import paths
+from rl.envs.env import EnvCNN
+from rl.wrapper.agent import AgentRunner
+from rl.wrapper.environment import EnvironmentWrapper
+from utils.mlflow_api import load_file
+
+if __name__ == '__main__':
+    mlflow.set_tracking_uri(paths.mlflow_path)
+    mlflow.set_experiment("Tests")
+
+    with mlflow.start_run():
+        data = load_file(run_id="f4bdae299f694599ba91c7dd1f77c9b5", fn="ticker.pkl", experiment="Datasets")
+
+        env = EnvironmentWrapper(EnvCNN, data)
+        env.create(max_episode_timesteps=max(len(tck) for tck in env.data))
+
+        agent = AgentRunner(env)
+        agent.create()
+
+        agent.train(episodes=4, episode_progress_indicator=env.len_data)
