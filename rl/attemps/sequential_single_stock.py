e7cfab2d2fb7f5aa80342dc5868a23efd76163dd
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-10-19 15:32:58 +0100

    new logger, added pred and eval interface

--- rl/attemps/sequential_single_stock.py
+++ rl/attemps/sequential_single_stock.py
@@ -2,15 +2,17 @@ import mlflow
 
 import paths
 from rl.envs.env import EnvCNN
-from rl.wrapper.agent import AgentActObserve, log_callback
+from rl.wrapper.agent import AgentActExperienceUpdate, AgentActObserve
 from rl.wrapper.environment import EnvironmentWrapper
-from utils.mlflow_api import load_file
+from utils.mlflow_api import load_file, MlflowAPI, log_file
+from utils.logger import setup_logger
 
 
-class AgentActObserveCustom(AgentActObserve):
+class CustomAgent(AgentActObserve):
 
     def episode_end_callback(self):
-        log_callback(self.env.tf_env)
+        self.log_callback(self.env.tf_env)
+        self.eval_callback()
 
 
 if __name__ == '__main__':
@@ -18,12 +20,17 @@ if __name__ == '__main__':
     mlflow.set_experiment("Tests")
 
     with mlflow.start_run():
-        data = load_file(run_id="f4bdae299f694599ba91c7dd1f77c9b5", fn="ticker.pkl", experiment="Datasets")
+        setup_logger("INFO")
+        data = load_file(run_id="582dd57030d74e7d8cf4f8fd2b1fb189", fn="ticker.pkl", experiment="Datasets")
 
         env = EnvironmentWrapper(EnvCNN, data)
         env.create(max_episode_timesteps=max(len(tck) for tck in env.data))
 
-        agent = AgentActObserveCustom(env)
-        agent.create()
+        agent = CustomAgent(env)
+        # agent.create()
+        agent.load(MlflowAPI(run_id="c3aaa7c52b3f41afb256c4c3ad4376f4").get_artifact_path())
 
-        agent.train(episodes=2, episode_progress_indicator=env.len_data)
+        # agent.train(episodes=6, episode_progress_indicator=env.len_data)
+        # agent.save()
+        pred = agent.predict()
+        log_file(pred, "pred.pkl")
