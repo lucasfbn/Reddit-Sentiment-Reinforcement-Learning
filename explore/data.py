1b511908cb6aab13bc66513c2947405323557921
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-11-07 20:49:55 +0000

    Update data.py

--- explore/data.py
+++ explore/data.py
@@ -6,6 +6,11 @@ from utils.mlflow_api import load_file
 mlflow.set_tracking_uri(paths.mlflow_path)
 mlflow.set_experiment("Tests")
 
-data = load_file(run_id="247244f584294534a0d758ba65ef1749", fn="ticker.pkl", experiment="Datasets")
+data = load_file(run_id="316b9d31ba41439ebc34c309f9a659ee", fn="ticker.pkl", experiment="Datasets")
+
+import pickle as pkl
+
+with open("temp.pkl", "wb") as f:
+    pkl.dump(data[:10], f)
 
 print()
