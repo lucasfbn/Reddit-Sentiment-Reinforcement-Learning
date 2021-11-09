c2daa0d5fad0ee5b86bbea18680c23676705b77c
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-11-09 20:01:23 +0000

    Ticker iteration is now handled by separate data iterator

--- rl/envs/env.py
+++ rl/envs/env.py
@@ -6,6 +6,7 @@ from tensorforce import Environment
 from preprocessing.sequences import Sequence
 from rl.envs.simple_trading import SimpleTradingEnvTraining
 from rl.envs.utils.reward_counter import RewardCounter
+from rl.envs.utils.data_iterator import DataIterator
 
 
 class Env(Environment):
@@ -13,21 +14,17 @@ class Env(Environment):
     def __init__(self, ticker):
         super().__init__()
 
-        self.ticker = ticker
-        self.ticker_iter = 0
-        self.ticker_iter_max = len(self.ticker)
+        self.data_iter = DataIterator(ticker)
 
-        self.curr_ticker = None
         self.curr_reward_counter = RewardCounter()
-        self.curr_sequences = None
         self.curr_simple_trading_env = SimpleTradingEnvTraining("init")
-        self.curr_sequence = None
-
-        self.episode_end = False
-        self.episode_count = 0
 
     @staticmethod
-    def get_state(sequence: Sequence):
+    def get_state_field(sequence: Sequence):
+        """
+        Implemented in subclasses. Determines which field of the sequence object
+        contains the state.
+        """
         raise NotImplementedError
 
     @staticmethod
@@ -37,25 +34,8 @@ class Env(Environment):
     def states(self):
         raise NotImplementedError
 
-    def next_ticker(self):
-        if self.ticker_iter == self.ticker_iter_max:
-            self.ticker_iter = 0
-
-        r = self.ticker[self.ticker_iter]
-
-        self.ticker_iter += 1
-        self.curr_ticker = r
-        self.curr_sequences = deque(self.curr_ticker.sequences)
-
-    def next_sequence(self):
-        if len(self.curr_sequences) == 0:
-            self.episode_end = True
-        else:
-            next_sequence = self.curr_sequences.popleft()
-            self.curr_sequence = next_sequence
-
-    def next_state(self):
-        next_state = self.get_state(self.curr_sequence)
+    def next_state(self, sequence):
+        next_state = self.get_state_field(sequence)
         next_state = self.shape_state(next_state)
         return next_state
 
@@ -72,7 +52,7 @@ class Env(Environment):
         return reward
 
     def execute(self, actions):
-        price = self.curr_sequence.price
+        price = self.data_iter.curr_sequence.price
 
         # Hold
         if actions == 0:
@@ -91,22 +71,22 @@ class Env(Environment):
 
         self.curr_reward_counter.add_reward(reward)
 
-        self.next_sequence()
-        next_state = self.next_state()
-        return next_state, self.episode_end, reward
+        next_sequence = self.data_iter.next_sequence()
+        next_state = self.next_state(next_sequence)
+        return next_state, self.data_iter.is_episode_end(), reward
 
     def reset(self):
-        self.episode_end = False
-        self.episode_count += 1
+        self.data_iter.episode_end = False
+        self.data_iter.episode_count += 1
 
-        self.next_ticker()
-        self.next_sequence()
-        state = self.next_state()
-        self.curr_simple_trading_env = SimpleTradingEnvTraining(ticker_name=self.curr_ticker.name)
+        self.data_iter.next_ticker()
+        next_sequence = self.data_iter.next_sequence()
+        state = self.next_state(next_sequence)
+        self.curr_simple_trading_env = SimpleTradingEnvTraining(ticker_name=self.data_iter.curr_ticker.name)
         return state
 
     def log(self):
-        self.curr_reward_counter.log(step=self.episode_count)
+        self.curr_reward_counter.log(step=self.data_iter.episode_count)
         self.curr_reward_counter = RewardCounter()
 
     def actions(self):
@@ -119,11 +99,11 @@ class Env(Environment):
 class EnvNN(Env):
 
     def states(self):
-        shape = self.ticker[0].sequences[0].flat.shape
+        shape = self.data_iter.ticker[0].sequences[0].flat.shape
         return dict(type="float", shape=(shape[1],), min_value=0.0, max_value=1.0)
 
     @staticmethod
-    def get_state(sequence: Sequence):
+    def get_state_field(sequence: Sequence):
         return sequence.flat
 
     @staticmethod
@@ -136,11 +116,11 @@ class EnvNN(Env):
 class EnvCNN(Env):
 
     def states(self):
-        shape = self.ticker[0].sequences[0].arr.shape
+        shape = self.data_iter.ticker[0].sequences[0].arr.shape
         return dict(type="float", shape=(1, shape[0], shape[1]), min_value=0.0, max_value=1.0)
 
     @staticmethod
-    def get_state(sequence: Sequence):
+    def get_state_field(sequence: Sequence):
         return sequence.arr
 
     @staticmethod
