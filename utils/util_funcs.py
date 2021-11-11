df9e6970744046aa206821bd837d89e045e28017
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-11-11 22:40:42 +0000

    Cleanup

--- utils/util_funcs.py
+++ utils/util_funcs.py
@@ -24,15 +24,6 @@ def update_check_key(input_dict, update_dict):
     return input_dict
 
 
-def drop_stats(func):
-    def wrapper(*args, **kwargs):
-        old_len = len(args[0].df)
-        func(*args, **kwargs)
-        log.info(f"{func.__name__} dropped {old_len - len(args[0].df)} items.")
-
-    return wrapper
-
-
 def dt_to_timestamp(time):
     if time is None: return None
     return int(time.timestamp())
