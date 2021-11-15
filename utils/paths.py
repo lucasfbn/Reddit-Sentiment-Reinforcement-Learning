3014da8ac43bb1fc1048f767c076033c36d2f29e
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-11-15 20:44:03 +0000

    Cleanup

--- utils/paths.py
+++ utils/paths.py
@@ -11,19 +11,8 @@ storage_path = base_path / "storage"
 data_path = storage_path / "data"
 
 mlflow_dir = Path("C:/project_data/Trendstuff/mlflow")
-mlflow_path = f"file:///{(mlflow_dir / 'mlruns').as_posix()}"
-
-artifact_path = lambda artifact_uri: Path("C:" + artifact_uri.split(":")[2])
 
 price_data_cache = storage_path / "price_data_cache.sqlite"
-
-
-def create_dir(path, fn="", suffix=0):
-    while os.path.exists(path / (fn + f"_{suffix}")):
-        suffix += 1
-    os.mkdir(path / (fn + f"_{suffix}"))
-    return path / (fn + f"_{suffix}")
-
 
 # sentiment analysis
 ticker_dir = storage_path / "ticker"
