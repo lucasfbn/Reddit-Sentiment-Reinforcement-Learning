e296bbaf0ed373fdb24413a9c54905cc29c569ad
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-11-30 16:59:13 +0000

    Dashboard now displays min and max date

--- rl/eval/stock/dashboard.py
+++ rl/eval/stock/dashboard.py
@@ -56,15 +56,19 @@ if mlflow_id != "":
 
     ticker_name = st.sidebar.selectbox(
         "Stock ticker", tuple(ticker_names),
-        index=1254
+        index=0
     )
     ticker_id = ticker_names.index(ticker_name)
 
     curr_ticker = point_data[ticker_id]
 
-    col1, col2 = st.columns(2)
+    col1, col2, col3, col4 = st.columns(4)
 
     col1.metric(label="Profit", value=round(curr_ticker["metadata"]["profit"], 3))
+    col2.metric(label="N_Open_Positions", value=curr_ticker["metadata"]["open_positions"])
+    col3.metric(label="Min Date", value=curr_ticker["metadata"]["min_date"])
+    col4.metric(label="Max Date", value=curr_ticker["metadata"]["max_date"])
+
     # col2.metric(label="Open Positions", value=curr_ticker["metadata"]["open_positions"])
 
     st.plotly_chart(plot_price_action(curr_ticker), use_container_width=True)
