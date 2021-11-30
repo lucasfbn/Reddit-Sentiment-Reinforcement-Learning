16d90ce8f860d12a9ffc64f1481d17baecd810a8
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-11-30 16:45:45 +0000

    Eval of single stock model, dashboard for visualizing, closes #122

---
+++ rl/eval/stock/dashboard.py
@@ -0,0 +1,70 @@
+import streamlit as st
+from mlflow_utils import artifact_path, init_mlflow, load_file
+from mlflow_utils import setup_logger
+import utils.paths
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+
+EXPERIMENT = "Tests"
+POINT_DATA_FN = "eval_stockwise.json"
+METADATA_FN = "agg_metadata.csv"
+AGG_FN = "agg_metadata_stats.csv"
+
+
+@st.cache
+def plot_price_action(ticker_data):
+    df = pd.DataFrame(ticker_data["points"])
+    df["actions"] = df["actions"].astype(str)
+
+    fig1 = px.scatter(x="timesteps", y="prices", data_frame=df, color="actions", hover_data=["rewards"])
+    fig1.update_traces(marker={"size": 20})
+
+    fig2 = px.line(x="timesteps", y="prices", data_frame=df)
+
+    fig3 = go.Figure(data=fig1.data + fig2.data)
+    fig3.update_layout(
+        title="",
+        xaxis_title="Timesteps",
+        yaxis_title="Price (scaled)",
+        legend_title="Actions",
+    )
+    fig3.update(layout_yaxis_range=[0, 1])
+
+    return fig3
+
+
+st.set_page_config(layout="wide")
+
+mlflow_id = st.sidebar.text_input(label="mlflow id", value="")
+
+if mlflow_id != "":
+    init_mlflow(utils.paths.mlflow_dir, "Tests")
+    setup_logger("INFO")
+
+    point_data = load_file(run_id=mlflow_id, fn=POINT_DATA_FN, experiment=EXPERIMENT)
+    metadata = load_file(run_id=mlflow_id, fn=METADATA_FN, experiment=EXPERIMENT)
+    agg = load_file(run_id=mlflow_id, fn=AGG_FN, experiment=EXPERIMENT)
+
+    with st.expander("Aggregated Statistics"):
+        col1, col2 = st.columns(2)
+
+        col1.dataframe(agg)
+        col2.dataframe(metadata)
+
+    ticker_names = [ticker["metadata"]["ticker"] for ticker in point_data]
+
+    ticker_name = st.sidebar.selectbox(
+        "Stock ticker", tuple(ticker_names),
+        index=1254
+    )
+    ticker_id = ticker_names.index(ticker_name)
+
+    curr_ticker = point_data[ticker_id]
+
+    col1, col2 = st.columns(2)
+
+    col1.metric(label="Profit", value=round(curr_ticker["metadata"]["profit"], 3))
+    # col2.metric(label="Open Positions", value=curr_ticker["metadata"]["open_positions"])
+
+    st.plotly_chart(plot_price_action(curr_ticker), use_container_width=True)
---
+++ rl/eval/stock/eval_ticker.py
@@ -0,0 +1,161 @@
+from tqdm import tqdm
+
+from rl.eval.utils.predict_proba import predict_proba
+import pandas as pd
+
+
+class Tracker:
+
+    def __init__(self, ticker_name):
+        self.ticker_name = ticker_name
+
+        self._actions = []
+        self._prices = []
+        self._rewards = []
+        self._timesteps = 0
+        self._metadata = []
+
+    def add(self, action, price, reward):
+        self._actions.append(action)
+        self._prices.append(price)
+        self._rewards.append(reward)
+        self._timesteps += 1
+
+    def add_metadata(self, info: dict):
+        self._metadata.append(info)
+
+    def make_dict(self):
+        d = {
+            "metadata": {
+                "ticker": self.ticker_name,
+                "profit": sum(self._rewards),
+            },
+            "points": {
+                "actions": self._actions,
+                "prices": self._prices,
+                "timesteps": list(range(self._timesteps)),
+                "rewards": self._rewards
+            }
+        }
+
+        for metadata in self._metadata:
+            d["metadata"] = d["metadata"] | metadata
+
+        return d
+
+
+class AggStats:
+
+    def __init__(self, tracker_df):
+        self.tracker_df = tracker_df
+
+    @staticmethod
+    def q(n):
+        def percentile_(x):
+            return x.quantile(n)
+
+        percentile_.__name__ = str(n)
+        return percentile_
+
+    @staticmethod
+    def pos(series):
+        return sum(series > 0) / len(series)
+
+    @staticmethod
+    def even(series):
+        return sum(series == 0) / len(series)
+
+    @staticmethod
+    def neg(series):
+        return sum(series < 0) / len(series)
+
+    def agg(self):
+        df = self.tracker_df.select_dtypes(include="number")
+        agg = df.agg(["count", "mean", "min", "max", self.q(0.25), self.q(0.50), self.q(0.75),
+                      self.q(0.9), self.q(0.95), self.pos, self.even, self.neg])
+        agg = agg.round(4)
+        agg["func"] = agg.index
+
+        cols = agg.columns.tolist()
+        agg = agg[cols[-1:] + cols[:-1]]
+        return agg
+
+
+class Eval:
+
+    def __init__(self, ticker, model, training_env_cls, trading_env_cls):
+        self.trading_env_cls = trading_env_cls
+        self.training_env_cls = training_env_cls
+        self.model = model
+        self.ticker = ticker
+
+        self._all_tracker = []
+
+    def _eval_sequence(self, sequence, trading_env):
+
+        state = self.training_env_cls.state_handler.forward(sequence,
+                                                            trading_env.inventory_state())
+        action, _ = predict_proba(self.model, state)
+
+        if action == 0:
+            reward = trading_env.hold(sequence.price)
+        elif action == 1:
+            reward = trading_env.buy(sequence.price)
+        elif action == 2:
+            reward = trading_env.sell(sequence.price)
+        else:
+            raise ValueError("Invalid action.")
+
+        return action, reward
+
+    def _eval_sequences(self, sequences, tracker):
+
+        trading_env = self.trading_env_cls()
+
+        for seq in sequences:
+            state = self.training_env_cls.state_handler.forward(seq,
+                                                                trading_env.inventory_state())
+            action, _ = predict_proba(self.model, state)
+
+            if action == 0:
+                reward = trading_env.hold(seq.price)
+            elif action == 1:
+                reward = trading_env.buy(seq.price)
+            elif action == 2:
+                reward = trading_env.sell(seq.price)
+            else:
+                raise ValueError("Invalid action.")
+
+            tracker.add(action, seq.price, reward)
+
+        tracker.add_metadata({"open_positions": len(trading_env.inventory)})
+
+    def eval_ticker(self):
+        for ticker in tqdm(self.ticker):
+            tracker = Tracker(ticker.name)
+
+            self._eval_sequences(ticker.sequences, tracker)
+
+            self._all_tracker.append(tracker)
+
+    @property
+    def all_tracker(self):
+        return self._all_tracker
+
+    @property
+    def all_tracker_dict(self):
+        return [tracker.make_dict() for tracker in self._all_tracker]
+
+    @property
+    def agg_metadata_df(self):
+        return pd.DataFrame([ticker["metadata"] for ticker in self.all_tracker_dict])
+
+    @property
+    def agg_metadata_stats(self):
+        agg_stats = AggStats(self.agg_metadata_df)
+        return agg_stats.agg()
+
+    @property
+    def agg_metadata_stats_flat(self):
+        return pd.json_normalize(self.agg_metadata_stats.drop(columns=["func"]).to_dict(),
+                                 sep="_").to_dict(orient="records")[0]
