72065cad15d4b616bec0ffdfb250be3080564238
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-12-01 17:56:31 +0000

    Addtional logging

--- rl/eval/stock/main.py
+++ rl/eval/stock/main.py
@@ -5,23 +5,23 @@ from stable_baselines3 import PPO
 
 import utils.paths
 from rl.eval.stock.eval_ticker import Eval
-from rl.train.envs.env import EnvCNN
+from rl.train.envs.env import EnvCNN, EnvCNNExtended
 from rl.train.envs.sub_envs.trading import SimpleTradingEnvEvaluation
 
 
-def main():
-    init_mlflow(utils.paths.mlflow_dir, "Tests")
+def main(dataset_run_id, model_run_id, model_fn):
+    init_mlflow(utils.paths.mlflow_dir, "Eval_Stocks")
     setup_logger("INFO")
     with mlflow.start_run():
-        data = load_file(run_id="5896df8aa22c41a3ade34d747bc9ed9a", fn="ticker.pkl", experiment="Datasets")
+        data = load_file(run_id=dataset_run_id, fn="ticker.pkl", experiment="Datasets")
 
-        training_env = EnvCNN
+        training_env = EnvCNNExtended
         trading_env = SimpleTradingEnvEvaluation
 
         model_path = (
-                artifact_path(run_id="658bd49019894ffc99f1c800070b0be4", experiment="Tests")
+                artifact_path(run_id=model_run_id, experiment="Training")
                 / "models"
-                / "rl_model_680052_steps.zip"
+                / model_fn
         )
         model = PPO.load(model_path)
 
@@ -32,6 +32,11 @@ def main():
         log_file(eval_env.agg_metadata_df, "agg_metadata.csv")
         log_file(eval_env.agg_metadata_stats, "agg_metadata_stats.csv")
         mlflow.log_params(eval_env.agg_metadata_stats_flat)
+        mlflow.log_params({"dataset_run_id": dataset_run_id, "model_run_id": model_run_id, "model_fn": model_fn})
 
 
-main()
+main(
+    dataset_run_id="c3925e0cbdcd4620b5cb909e1a629419",
+    model_run_id="a8ed39bab664416882c4f52d63c3a9bb",
+    model_fn="rl_model_646630_steps.zip"
+)
