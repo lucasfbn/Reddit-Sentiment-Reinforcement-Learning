c018f631a921525aaf71f7c09da452c1262f8d49
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-12-01 17:57:17 +0000

    Changed linear layer

--- rl/train/networks/cnn_1d.py
+++ rl/train/networks/cnn_1d.py
@@ -30,20 +30,24 @@ class CustomCNN(BaseFeaturesExtractor):
                 th.as_tensor(observation_space.sample()[None]).float()
             ).shape[1]
 
-        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+        self.linear = nn.Sequential(nn.Linear(n_flatten, n_flatten // 2), nn.ReLU(),
+                                    nn.Linear(n_flatten // 2, n_flatten // 4), nn.ReLU())
 
     def forward(self, observations: th.Tensor) -> th.Tensor:
         return self.linear(self.cnn(observations))
 
-# from gym import Env, spaces
-# import numpy as np
-#
-# shape = (10, 14)
-#
-# obs_space = spaces.Box(low=np.zeros(shape),
-#                        high=np.ones(shape),
-#                        dtype=np.float64)
-#
-# model = nn.Conv1d(in_channels=obs_space.shape[0], out_channels=32, kernel_size=7, stride=1, padding="same")
-# out = model(th.as_tensor(obs_space.sample()[None]).float())
-# print(out)
+
+if __name__ == '__main__':
+    from gym import Env, spaces
+    import numpy as np
+
+    shape = (10, 14)
+
+    obs_space = spaces.Box(low=np.zeros(shape),
+                           high=np.ones(shape),
+                           dtype=np.float64)
+
+    model = CustomCNN(obs_space)
+    # model = nn.Conv1d(in_channels=obs_space.shape[0], out_channels=32, kernel_size=5, stride=1, padding=0)
+    out = model(th.as_tensor(obs_space.sample()[None]).float())
+    print(out)
