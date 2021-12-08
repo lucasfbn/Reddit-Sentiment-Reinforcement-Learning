f3830d38712beee17ab8d40e855c3c1a6dc3f1e9
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-12-08 22:30:07 +0000

    Ticker class can now hold eval data

--- preprocessing/ticker.py
+++ preprocessing/ticker.py
@@ -1,11 +1,28 @@
+from dataclasses import dataclass, asdict
+
+import pandas as pd
+
+
+@dataclass
+class Eval:
+    reward: float = None
+    open_positions: int = None
+    min_date = pd.datetime = None
+    max_date = pd.datetime = None
+
+    def to_dict(self):
+        return asdict(self)
+
+
 class Ticker:
 
-    def __init__(self, name, df):
+    def __init__(self, name, df=None):
         self.df = df
         self.name = name
         self.exclude = False
 
         self.sequences = None
+        self.eval = Eval()
 
     def __len__(self):
         return len(self.sequences)
