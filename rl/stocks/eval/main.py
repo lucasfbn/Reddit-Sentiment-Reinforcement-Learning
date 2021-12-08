b85603fddebc7a21e6581ae4e648523165640e2b
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-12-08 15:57:30 +0000

    logging into eval stocks again

--- rl/stocks/eval/main.py
+++ rl/stocks/eval/main.py
@@ -10,9 +10,9 @@ from rl.stocks.train.envs.sub_envs.trading import SimpleTradingEnvEvaluation
 
 
 def main(dataset_run_id, model_run_id, model_fn):
-    init_mlflow(utils.paths.mlflow_dir, "Training")
+    init_mlflow(utils.paths.mlflow_dir, "Eval_Stocks")
     setup_logger("INFO")
-    with mlflow.start_run(run_id=model_run_id, nested=True):
+    with mlflow.start_run():
         data = load_file(run_id=dataset_run_id, fn="ticker.pkl", experiment="Datasets")
 
         training_env = EnvCNNExtended
@@ -37,6 +37,6 @@ def main(dataset_run_id, model_run_id, model_fn):
 
 main(
     dataset_run_id="c3925e0cbdcd4620b5cb909e1a629419",
-    model_run_id="0e29194a80494ad1a0ad03ab3b0eb30b",
+    model_run_id="d0c7a104aad64e25b267021078a14781",
     model_fn="rl_model_3233150_steps.zip"
 )
