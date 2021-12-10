04ac0e62ea627652fa9a6f277ccafb182e09866c
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-12-10 20:41:50 +0000

    Removed properties

--- rl/stocks/eval/eval_ticker.py
+++ rl/stocks/eval/eval_ticker.py
@@ -1,7 +1,6 @@
 from tqdm import tqdm
 
 from rl.utils.predict_proba import predict_proba
-import pandas as pd
 
 
 class Eval:
@@ -11,8 +10,6 @@ class Eval:
         self.training_env_cls = training_env_cls
         self.model = model
         self.ticker = ticker
-
-        self._all_tracker = []
 
     def _eval_sequences(self, sequences):
 
@@ -46,25 +43,3 @@ class Eval:
 
         _ = [(ticker.drop_sequence_data(), ticker.aggregate_rewards()) for ticker in self.ticker]
         return self.ticker
-
-    @property
-    def all_tracker(self):
-        return self._all_tracker
-
-    @property
-    def all_tracker_dict(self):
-        return [tracker.make_dict() for tracker in self._all_tracker]
-
-    @property
-    def agg_metadata_df(self):
-        return pd.DataFrame([ticker["metadata"] for ticker in self.all_tracker_dict])
-
-    @property
-    def agg_metadata_stats(self):
-        agg_stats = AggStats(self.agg_metadata_df)
-        return agg_stats.agg()
-
-    @property
-    def agg_metadata_stats_flat(self):
-        return pd.json_normalize(self.agg_metadata_stats.drop(columns=["func"]).to_dict(),
-                                 sep="_").to_dict(orient="records")[0]
