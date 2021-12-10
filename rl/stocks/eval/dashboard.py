6da6a1f561497290a9e8eaf66745efb99d0384d3
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-12-10 22:03:26 +0000

    Updated dashboard to use seq obj

--- rl/stocks/eval/dashboard.py
+++ rl/stocks/eval/dashboard.py
@@ -6,23 +6,24 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 
-EXPERIMENT = "Tests"
-POINT_DATA_FN = "eval_stockwise.json"
-METADATA_FN = "agg_metadata.csv"
-AGG_FN = "agg_metadata_stats.csv"
+EXPERIMENT = "Eval_Stocks"
+EVAL_TICKER_FN = "evl_ticker.pkl"
+AGG_EVAL_DF_FN = "agg_evl_df.csv"
+AGG_STATS_FN = "agg_stats.csv"
 
 
 @st.cache
-def plot_price_action(ticker_data):
-    df = pd.DataFrame(ticker_data["points"])
-    df["actions"] = df["actions"].astype(str)
+def plot_price_action(ticker):
+    df = pd.DataFrame([seq.evl.to_dict() | seq.metadata.to_dict() for seq in ticker.sequences])
+    df["date"] = df["date"].astype(str)
+    df["action"] = df["action"].astype(str)
 
-    fig1 = px.scatter(x="timesteps", y="prices", data_frame=df, color="actions", hover_data=["rewards", "dates"],
+    fig1 = px.scatter(x=df.index, y="price", data_frame=df, color="action", hover_data=["reward", "date"],
                       color_discrete_map={'2': 'rgb(255,0,0)', '1': 'rgb(0,255,0)',
                                           '0': 'rgb(0,0,255)'})
     fig1.update_traces(marker={"size": 20})
 
-    fig2 = px.line(x="timesteps", y="prices", data_frame=df)
+    fig2 = px.line(x=df.index, y="price", data_frame=df)
 
     fig3 = go.Figure(data=fig1.data + fig2.data)
     fig3.update_layout(
@@ -44,17 +45,17 @@ if mlflow_id != "":
     init_mlflow(utils.paths.mlflow_dir, "Tests")
     setup_logger("INFO")
 
-    point_data = load_file(run_id=mlflow_id, fn=POINT_DATA_FN, experiment=EXPERIMENT)
-    metadata = load_file(run_id=mlflow_id, fn=METADATA_FN, experiment=EXPERIMENT)
-    agg = load_file(run_id=mlflow_id, fn=AGG_FN, experiment=EXPERIMENT)
+    eval_ticker = load_file(run_id=mlflow_id, fn=EVAL_TICKER_FN, experiment=EXPERIMENT)
+    agg_evl_df = load_file(run_id=mlflow_id, fn=AGG_EVAL_DF_FN, experiment=EXPERIMENT)
+    agg_stats = load_file(run_id=mlflow_id, fn=AGG_STATS_FN, experiment=EXPERIMENT)
 
     with st.expander("Aggregated Statistics"):
         col1, col2 = st.columns(2)
 
-        col1.dataframe(agg)
-        col2.dataframe(metadata)
+        col1.dataframe(agg_stats)
+        col2.dataframe(agg_evl_df)
 
-    ticker_names = [ticker["metadata"]["ticker"] for ticker in point_data]
+    ticker_names = [ticker.name for ticker in eval_ticker]
 
     ticker_name = st.sidebar.selectbox(
         "Stock ticker", tuple(ticker_names),
@@ -62,15 +63,13 @@ if mlflow_id != "":
     )
     ticker_id = ticker_names.index(ticker_name)
 
-    curr_ticker = point_data[ticker_id]
+    curr_ticker = eval_ticker[ticker_id]
 
     col1, col2, col3, col4 = st.columns(4)
 
-    col1.metric(label="Profit", value=round(curr_ticker["metadata"]["profit"], 3))
-    col2.metric(label="N_Open_Positions", value=curr_ticker["metadata"]["open_positions"])
-    col3.metric(label="Min Date", value=curr_ticker["metadata"]["min_date"])
-    col4.metric(label="Max Date", value=curr_ticker["metadata"]["max_date"])
-
-    # col2.metric(label="Open Positions", value=curr_ticker["metadata"]["open_positions"])
+    col1.metric(label="Profit", value=round(curr_ticker.evl.reward, 3))
+    col2.metric(label="N_Open_Positions", value=curr_ticker.evl.open_positions)
+    col3.metric(label="Min Date", value=str(curr_ticker.evl.min_date))
+    col4.metric(label="Max Date", value=str(curr_ticker.evl.max_date))
 
     st.plotly_chart(plot_price_action(curr_ticker), use_container_width=True)
