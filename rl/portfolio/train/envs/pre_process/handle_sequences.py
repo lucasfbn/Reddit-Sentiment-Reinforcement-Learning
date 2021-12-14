8a0bfef3d6da0d8167d2c22e405433cee53a310c
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-12-14 18:10:42 +0000

    Sequences are now shuffled within env (and not sorted anymore)

--- rl/portfolio/train/envs/pre_process/handle_sequences.py
+++ rl/portfolio/train/envs/pre_process/handle_sequences.py
@@ -10,8 +10,3 @@ def remove_invalid_sequences(sequences):
     sequences = [seq for seq in sequences if seq.evl.reward_backtracked is not None]
     sequences = [seq for seq in sequences if seq.evl.action == 1]
     return sequences
-
-
-def order_sequences_daywise(sequences):
-    buys = [seq for seq in sequences if seq.evl.action == 1]
-    return sorted(buys, key=lambda seq: (seq.metadata.date, 1 - seq.evl.buy_proba))
