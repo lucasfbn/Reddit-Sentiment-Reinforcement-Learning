86f8a93ac7ecc7159c642b142648ab86fa06af0e
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-12-14 18:10:00 +0000

    Moved sub functions to appriopriate functions

--- rl/portfolio/train/envs/pre_process/handle_sequences.py
+++ rl/portfolio/train/envs/pre_process/handle_sequences.py
@@ -7,7 +7,9 @@ def get_all_sequences(ticker):
 
 
 def remove_invalid_sequences(sequences):
-    return [seq for seq in sequences if seq.evl.reward_backtracked is not None]
+    sequences = [seq for seq in sequences if seq.evl.reward_backtracked is not None]
+    sequences = [seq for seq in sequences if seq.evl.action == 1]
+    return sequences
 
 
 def order_sequences_daywise(sequences):
