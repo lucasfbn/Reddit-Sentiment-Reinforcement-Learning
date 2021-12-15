25e1db983060fba6225b2888215cac279dffe206
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-12-15 15:11:52 +0000

    Ticker name is now merged (may be removed later when eval is run on updated dataset)

--- rl/portfolio/train/envs/pre_process/merge_ticker.py
+++ rl/portfolio/train/envs/pre_process/merge_ticker.py
@@ -5,5 +5,6 @@ def merge_ticker(data_ticker, evl_ticker):
 
         for d_seq, e_seq in zip(list(d_ticker.sequences), list(e_ticker.sequences)):
             e_seq.data = d_seq.data
+            e_seq.metadata.ticker_name = d_seq.metadata.ticker_name
 
     return evl_ticker
