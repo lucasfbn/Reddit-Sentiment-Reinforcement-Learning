0cd6f893be16e167969b8b0a6144925aa39a487a
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-12-15 15:37:31 +0000

    Now uses custom network, uses mlflow now

--- rl/portfolio/train/main.py
+++ rl/portfolio/train/main.py
@@ -1,36 +1,40 @@
-from mlflow_utils import init_mlflow, load_file
+import mlflow
+from mlflow_utils import init_mlflow, load_file, setup_logger
 from stable_baselines3 import PPO
 
 import rl.portfolio.train.envs.pre_process.handle_sequences as hs
 from rl.portfolio.train.envs.env import EnvCNNExtended
 from rl.portfolio.train.envs.pre_process.merge_ticker import merge_ticker
-from rl.stocks.train.networks.cnn_1d import CustomCNN
+from rl.portfolio.train.networks.cnn_1d import CustomCNN
 from utils.paths import mlflow_dir
 
 
 def main(data_run_id, eval_run_id):
     init_mlflow(mlflow_dir, "Tests")
 
-    data = load_file(run_id=data_run_id, fn="ticker.pkl", experiment="Datasets")
-    evl = load_file(run_id=eval_run_id, fn="evl_ticker.pkl", experiment="Eval_Stocks")
+    with mlflow.start_run():
+        setup_logger("DEBUG")
 
-    merged = merge_ticker(data, evl)
-    all_sequences = hs.get_all_sequences(merged)
-    all_sequences = hs.remove_invalid_sequences(all_sequences)
+        data = load_file(run_id=data_run_id, fn="ticker.pkl", experiment="Datasets")
+        evl = load_file(run_id=eval_run_id, fn="evl_ticker.pkl", experiment="Eval_Stocks")
 
-    total_timesteps_p_episode = len(all_sequences)
+        merged = merge_ticker(data, evl)
+        all_sequences = hs.get_all_sequences(merged)
+        all_sequences = hs.remove_invalid_sequences(all_sequences)
 
-    episodes = 10
+        total_timesteps_p_episode = len(all_sequences)
 
-    env = EnvCNNExtended(all_sequences)
+        episodes = 10
 
-    policy_kwargs = dict(
-        features_extractor_class=CustomCNN,
-        features_extractor_kwargs=dict(features_dim=32)
-    )
+        env = EnvCNNExtended(all_sequences)
 
-    model = PPO('CnnPolicy', env, verbose=1, policy_kwargs=policy_kwargs)
-    model.learn(episodes * total_timesteps_p_episode + 1)
+        policy_kwargs = dict(
+            features_extractor_class=CustomCNN,
+            features_extractor_kwargs=dict(features_dim=128)
+        )
+
+        model = PPO('CnnPolicy', env, verbose=0, policy_kwargs=policy_kwargs)
+        model.learn(episodes * total_timesteps_p_episode + 1)
 
 
 main(data_run_id="0643613545e44e75b8017b9973598fb4", eval_run_id="f384f58217114433875eda44495272ad")
