4a8e7f4d0be77969834005b976e222cd177a738e
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-12-20 20:38:29 +0000

    Simplified ForcedEpisodeRewardHandler

--- tests/test_utils/test_forced_episode_reward_handler.py
+++ tests/test_utils/test_forced_episode_reward_handler.py
@@ -5,7 +5,7 @@ from rl.portfolio.train.envs.utils.forced_episode_reward_handler import ForcedEp
 
 def test_base():
     ForcedEpisodeRewardHandler.INITIAL_REWARD_WHEN_FORCED_EPISODE_END = -25.0
-    e = ForcedEpisodeRewardHandler(10, 100)
+    e = ForcedEpisodeRewardHandler(90)
     assert e.get_episode_end_reward(0) == -25.0
     assert e.get_episode_end_reward(90) == -0.0
     assert e.get_episode_end_reward(45) == -12.5
