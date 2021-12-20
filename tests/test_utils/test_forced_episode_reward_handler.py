2b0ab6bb3e2873aa7620543c2ae432d6f2e4c7a4
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2021-12-20 20:06:56 +0000

    Implemented ForcedEpisodeRewardHandler

---
+++ tests/test_utils/test_forced_episode_reward_handler.py
@@ -0,0 +1,14 @@
+import pytest
+
+from rl.portfolio.train.envs.utils.forced_episode_reward_handler import ForcedEpisodeRewardHandler
+
+
+def test_base():
+    ForcedEpisodeRewardHandler.INITIAL_REWARD_WHEN_FORCED_EPISODE_END = -25.0
+    e = ForcedEpisodeRewardHandler(10, 100)
+    assert e.get_episode_end_reward(0) == -25.0
+    assert e.get_episode_end_reward(90) == -0.0
+    assert e.get_episode_end_reward(45) == -12.5
+
+    with pytest.raises(ValueError):
+        assert e.get_episode_end_reward(91)
