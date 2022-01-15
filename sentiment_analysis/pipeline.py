3624d39a6887ecb887ff12ce461aa353dfdd590c
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-15 10:50:14 +0000

    migrated

--- sentiment_analysis/pipeline.py
+++ sentiment_analysis/pipeline.py
@@ -1,14 +1,12 @@
-import mlflow
-from mlflow_utils import log_file
-from mlflow_utils import setup_logger, init_mlflow
+import wandb
 from simplepipeline import seq_map
 
 from sentiment_analysis.tasks import *
 from utils.util_funcs import update_check_key
+from utils.wandb_utils import log_file
 
 params = {
     "gc_dump_fn": "gc_dump.csv",
-    "report_fn": "report.csv",
     "start": None,
     "end": None,
     "check_duplicates": True,
@@ -32,9 +30,9 @@ def pipeline(**kwargs):
     params = update_check_key(params, kwargs)
 
     df = get_from_gc(params["start"], params["end"], params["check_duplicates"], params["fields_to_retrieve"]).run()
-    log_file(df, params["gc_dump_fn"])
+    log_file(df, params["gc_dump_fn"], type="sentiment_analysis")
     gaps = retrieve_gaps(df).run()
-    log_file(gaps, "gaps.json")
+    log_file(gaps, "gaps.json", type="sentiment_analysis")
     df = filter_removed(df, params["cols_to_check_if_removed"]).run()
     df = add_temporal_informations(df).run()
     df = filter_authors(df,
@@ -55,15 +53,13 @@ def pipeline(**kwargs):
     timespans = retrieve_timespans(flattened_ticker_df, params["relevant_timespan_cols"]).run()
     timespans = seq_map(aggregate_submissions_per_timespan, timespans).run()
     df = summarize_timespans(timespans).run()
-    log_file(df, params["report_fn"])
-    mlflow.log_params(params=params)
+    log_file(df, "dataset.csv", type="sentiment_analysis")
+    wandb.log(params=params)
     return df
 
 
 def main():
-    init_mlflow(paths.mlflow_dir, "Sentiment")
-    with mlflow.start_run():
-        setup_logger("DEBUG")
+    with wandb.init(project="Trendstuff", group="Sentiment Analysis") as run:
         pipeline(start=datetime(year=2021, month=3, day=2), end=datetime(year=2021, month=11, day=29))
 
 
