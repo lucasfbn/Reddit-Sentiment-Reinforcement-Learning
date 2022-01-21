e26b6a9de44a9ef387e06938163e0b91371a34de
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-21 18:14:20 +0000

    Untaggled main function into load and train just like in 4597a41c15bf31819eb52daaf4628206283b2381

--- rl/stocks/train/main.py
+++ rl/stocks/train/main.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import wandb
@@ -8,7 +9,7 @@ from stable_baselines3.common.callbacks import (CheckpointCallback,
 from rl.stocks.train.callbacks.callbacks import EpisodeEndCallback
 from rl.stocks.train.envs.env import EnvCNNExtended
 from rl.stocks.train.envs.sub_envs.trading import SimpleTradingEnvTraining
-from rl.stocks.train.networks.cnn_1d_tune import Networks, TuneableNetwork
+from rl.stocks.train.networks.multi_input import Network
 from utils.wandb_utils import load_artefact
 
 """
@@ -17,42 +18,55 @@ https://stable-baselines.readthedocs.io/en/master/guide/callbacks.html
 https://pytorch.org/docs/stable/generated/torch.nn.Conv1d.html
 """
 
-if __name__ == '__main__':
-    with wandb.init(project="Trendstuff", group="RL_Stocks") as run:
-        data = load_artefact(run, "dataset.pkl", 0, "Dataset")
 
-        wandb.log(dict(ENABLE_TRANSACTION_COSTS=SimpleTradingEnvTraining.ENABLE_TRANSACTION_COSTS,
-                       ENABLE_NEG_BUY_REWARD=SimpleTradingEnvTraining.ENABLE_NEG_BUY_REWARD,
-                       ENABLE_POS_SELL_REWARD=SimpleTradingEnvTraining.ENABLE_POS_SELL_REWARD,
-                       TRANSACTION_FEE_BID=SimpleTradingEnvTraining.TRANSACTION_FEE_BID,
-                       TRANSACTION_FEE_ASK=SimpleTradingEnvTraining.TRANSACTION_FEE_ASK,
-                       HOLD_REWARD_MULTIPLIER=SimpleTradingEnvTraining.HOLD_REWARD_MULTIPLIER,
-                       PARTIAL_HOLD_REWARD=SimpleTradingEnvTraining.PARTIAL_HOLD_REWARD,
-                       dataset_id="5896df8aa22c41a3ade34d747bc9ed9a"))
+def load_data(data_version):
+    with wandb.init(project="Trendstuff", group="Throwaway") as run:
+        data = load_artefact(run, "dataset.pkl", data_version, "Dataset")
 
-        len_sequences = [len(tck) for tck in data]
-        max_timesteps = max(len_sequences)
-        total_timesteps_p_episode = sum(len_sequences)
+    return data
 
-        episodes = 10
+
+def train(data, env, run_dir, network, features_extractor_kwargs, num_steps, shutdown=False):
+    len_sequences = [len(tck) for tck in data]
+    max_timesteps = max(len_sequences)
+    total_timesteps_p_episode = sum(len_sequences)
+
+    wandb.log(dict(ENABLE_TRANSACTION_COSTS=SimpleTradingEnvTraining.ENABLE_TRANSACTION_COSTS,
+                   ENABLE_NEG_BUY_REWARD=SimpleTradingEnvTraining.ENABLE_NEG_BUY_REWARD,
+                   ENABLE_POS_SELL_REWARD=SimpleTradingEnvTraining.ENABLE_POS_SELL_REWARD,
+                   TRANSACTION_FEE_BID=SimpleTradingEnvTraining.TRANSACTION_FEE_BID,
+                   TRANSACTION_FEE_ASK=SimpleTradingEnvTraining.TRANSACTION_FEE_ASK,
+                   HOLD_REWARD_MULTIPLIER=SimpleTradingEnvTraining.HOLD_REWARD_MULTIPLIER,
+                   PARTIAL_HOLD_REWARD=SimpleTradingEnvTraining.PARTIAL_HOLD_REWARD))
+
+    policy_kwargs = dict(
+        features_extractor_class=network,
+        features_extractor_kwargs=features_extractor_kwargs
+    )
+
+    log_callback = EveryNTimesteps(n_steps=total_timesteps_p_episode, callback=EpisodeEndCallback())
+    checkpoint_callback = CheckpointCallback(save_freq=total_timesteps_p_episode,
+                                             save_path=Path(Path(run_dir) / "models").as_posix())
+
+    model = PPO('MultiInputPolicy', env, verbose=1, policy_kwargs=policy_kwargs,
+                tensorboard_log=(Path(run_dir) / "tensorboard").as_posix())
+    model.learn(num_steps * total_timesteps_p_episode + 1, callback=[log_callback, checkpoint_callback])
+
+    if shutdown:
+        os.system('shutdown -s -t 600')
+
+
+def main():
+    data = load_data(0)
+
+    with wandb.init(project="Trendstuff", group="Throwaway") as run:
+        wandb.tensorboard.patch(save=False)
 
         env = EnvCNNExtended(data)
 
-        networks = Networks(in_channels=env.observation_space.shape[0]).networks
+        train(data, env, num_steps=15, run_dir=run.dir,
+              network=Network, features_extractor_kwargs=dict(features_dim=128))
 
-        for network, features_dim in networks:
-            log_callback = EveryNTimesteps(n_steps=total_timesteps_p_episode, callback=EpisodeEndCallback())
-            checkpoint_callback = CheckpointCallback(save_freq=total_timesteps_p_episode,
-                                                     save_path=Path(Path(run.dir) / "models").as_posix())
 
-            policy_kwargs = dict(
-                features_extractor_class=TuneableNetwork,
-                features_extractor_kwargs=dict(cnn=network, features_dim=features_dim)
-            )
-
-            model = PPO('CnnPolicy', env, verbose=1, policy_kwargs=policy_kwargs)
-            model.learn(episodes * total_timesteps_p_episode + 1, callback=[log_callback, checkpoint_callback])
-
-        # import os
-        #
-        # os.system('shutdown -s')
+if __name__ == '__main__':
+    main()
