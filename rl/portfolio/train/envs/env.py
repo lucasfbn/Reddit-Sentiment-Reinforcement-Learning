b0b2434e4eeafa4841f32320ab4dc5cb8cee3e32
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-21 20:26:57 +0000

    Adapted to new RewardHandler()

--- rl/portfolio/train/envs/env.py
+++ rl/portfolio/train/envs/env.py
@@ -64,17 +64,18 @@ class BaseEnv(Env, ABC):
 
         intermediate_episode_end = self._trading_env.trades_exhausted()
 
-        reward_handler = RewardHandler(base_reward=reward)
-        reward_handler.negate_if_no_success(success)
-        reward_handler.discount_cash_bound(seq.evl.days_cash_bound)
+        reward_handler = RewardHandler()
+        reward = reward_handler.negate_if_no_success(reward, success)
+        reward = reward_handler.discount_cash_bound(reward, seq.evl.days_cash_bound)
 
-        reward_completed_steps = reward_handler.add_reward_completed_steps(self._data_iter.perc_completed_steps)
-        reward_discount_n_trades_left = reward_handler.discount_n_trades_left(self._trading_env.n_trades_left_scaled)
+        reward_completed_steps = reward_handler.add_reward_completed_steps(reward, self._data_iter.perc_completed_steps)
+        reward_discount_n_trades_left = reward_handler.discount_n_trades_left(reward_completed_steps,
+                                                                              self._trading_env.n_trades_left_scaled)
 
-        reward_handler.penalize_forced_episode_end(intermediate_episode_end)
-        reward_handler.reward_total_episode_end(episode_end)
+        total_reward = reward_handler.penalize_forced_episode_end(reward_discount_n_trades_left,
+                                                                  intermediate_episode_end)
+        total_reward = reward_handler.reward_total_episode_end(total_reward, episode_end)
 
-        total_reward = reward_handler.reward
         episode_end = bool(max(int(intermediate_episode_end), int(episode_end)))
 
         next_sequence, _, _ = next(self._next_state_iter)
