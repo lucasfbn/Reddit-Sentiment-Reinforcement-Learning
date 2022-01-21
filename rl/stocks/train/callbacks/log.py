9575059fd94709f13f6cef4ab81e797d9b8a02c5
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-21 22:41:38 +0000

    Created log_func

--- rl/stocks/train/callbacks/log.py
+++ rl/stocks/train/callbacks/log.py
@@ -1,2 +1,7 @@
 def log_func(full_df, episode_end_df):
-    pass
+    return dict(
+        sum=full_df["reward"].sum(),
+        even=sum(full_df["reward"] == 0.0) / len(full_df),
+        pos=sum(full_df["reward"] > 0.0) / len(full_df),
+        neg=sum(full_df["reward"] < 0.0) / len(full_df)
+    )
