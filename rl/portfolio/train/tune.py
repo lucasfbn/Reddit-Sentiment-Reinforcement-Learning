c8682556e0b90130ecaffa1fd67076d5a590c356
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-21 20:19:23 +0000

    Revert "updated to flat reward"
    This reverts commit a68da70948a31618e74b6553e79cc4216663cc51.

--- rl/portfolio/train/tune.py
+++ rl/portfolio/train/tune.py
@@ -13,11 +13,12 @@ data = load_data(0, 0)
 
 def objective(trial):
     global data
-    RewardHandler.FLAT_REWARD = trial["FLAT_REWARD"]
+    RewardHandler.TOTAL_EPISODE_END_REWARD = trial["TOTAL_EPISODE_END_REWARD"]
+    RewardHandler.COMPLETED_STEPS_MAX_REWARD = trial["COMPLETED_STEPS_MAX_REWARD"]
 
     env = EnvCNNExtended(data)
 
-    with wandb.init(project="Trendstuff", group="RL Portfolio Tune Rewards 2", job_type="runs") as run:
+    with wandb.init(project="Trendstuff", group="RL Portfolio Tune Rewards 1", job_type="runs") as run:
         wandb.tensorboard.patch(save=False)
         wandb.log(trial)
 
@@ -28,22 +29,27 @@ def objective(trial):
 
         last_n_episodes = pd.concat(e.to_df(include_last=False) for e in tracked_data[:-10])
         reward = last_n_episodes["reward"].median()
-        wandb.log({"reward_n": reward})
+        reward_completed_steps = last_n_episodes["reward_completed_steps"].median()
+        reward_discount_n_trades_left = last_n_episodes["reward_discount_n_trades_left"].median()
+        wandb.log({"reward_n": reward,
+                   "reward_completed_steps_n": reward_completed_steps,
+                   "reward_discount_n_trades_left_n": reward_discount_n_trades_left})
         tune.report(reward)
 
 
 if __name__ == "__main__":
     trial = {
-        "FLAT_REWARD": tune.grid_search([0.25, 0.5, 0.75, 1, 2, 3])
+        "TOTAL_EPISODE_END_REWARD": tune.grid_search(list(range(5, 11))),
+        "COMPLETED_STEPS_MAX_REWARD": tune.grid_search(list(range(1, 6)))
     }
 
     analysis = tune.run(
         objective,
         config=trial,
         mode="max",
-        num_samples=1,
+        num_samples=25,
         resources_per_trial={"cpu": 2}
     )
 
-    with wandb.init(project="Trendstuff", group="RL Portfolio Tune Rewards 2", job_type="overview") as run:
+    with wandb.init(project="Trendstuff", group="RL Portfolio Tune Rewards 1", job_type="overview") as run:
         run.log({"overview_rl_portfolio_tune_rewards": wandb.Table(dataframe=analysis.results_df)})
