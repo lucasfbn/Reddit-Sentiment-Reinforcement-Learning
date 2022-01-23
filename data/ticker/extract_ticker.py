46eb9a15a83909be43c26c893410932c87ce04ea
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-23 16:15:35 +0000

    Refactor: folder: storage -> data

---
+++ data/ticker/extract_ticker.py
@@ -0,0 +1,22 @@
+import os
+
+import pandas as pd
+
+from utils import paths
+
+df = None
+first = True
+for filename in os.listdir(paths.ticker_folder):
+    if first:
+        df = pd.read_csv(paths.ticker_folder / filename, sep="\t")
+        df = df[["Symbol"]]
+        df["exchange"] = filename.split(".")[0]
+        first = False
+    else:
+        next_df = pd.read_csv(paths.ticker_folder / filename, sep="\t")
+        next_df = next_df[["Symbol"]]
+        next_df["exchange"] = filename.split(".")[0]
+        df = df.append(next_df)
+
+df = df.sort_values(by=["Symbol"])
+df.to_csv(paths.all_ticker, sep=";", index=False)
--- utils/paths.py
+++ utils/paths.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 """
@@ -7,14 +6,11 @@ Used to avoid absolute paths throughout the project.
 
 base_path = Path(__file__).parent.parent
 tests_path = base_path / "tests"
-storage_path = base_path / "storage"
-data_path = storage_path / "data"
+data_path = base_path / "data"
 
-mlflow_dir = Path("C:/project_data/Trendstuff/mlflow")
-
-price_data_cache = storage_path / "price_data_cache.sqlite"
+price_data_cache = data_path / "price_data_cache.sqlite"
 
 # sentiment analysis
-ticker_dir = storage_path / "ticker"
+ticker_dir = data_path / "ticker"
 ticker_files = ticker_dir / "files"
 all_ticker = ticker_dir / "ticker.csv"
