a40a2b88bb68bd58acb67144f2f37256f066594c
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-23 13:14:48 +0000

    Can now handle dicts

--- rl/utils/predict_proba.py
+++ rl/utils/predict_proba.py
@@ -4,7 +4,14 @@ from stable_baselines3.common.policies import obs_as_tensor
 
 def predict_proba(model, state):
     obs = obs_as_tensor(state, model.policy.device)
-    obs = obs.unsqueeze(0)
+
+    # Add batch dimension
+    if isinstance(obs, dict):
+        for key, value in obs.items():
+            obs[key] = value.unsqueeze(0)
+    else:
+        obs = obs.unsqueeze(0)
+
     dis = model.policy.get_distribution(obs)
     probs = dis.distribution.probs
     probs_np = probs.detach().numpy()
