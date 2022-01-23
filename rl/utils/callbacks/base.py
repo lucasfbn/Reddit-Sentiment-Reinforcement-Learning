029c19568e6c41f7e55da1e83fa049d297759854
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-23 16:11:34 +0000

    Improved logger

---
+++ rl/utils/callbacks/base.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from stable_baselines3.common.callbacks import BaseCallback
+
+
+class Episode:
+
+    def __init__(self):
+        self.data = []
+
+    def to_df(self, include_last=True):
+        if include_last:
+            return pd.DataFrame(self.data)
+        else:
+            return pd.DataFrame(self.data[:-1])
+
+    def __len__(self):
+        return len(self.data)
+
+
+class Callback(BaseCallback):
+
+    def __init__(self, episodes_log_interval):
+        super().__init__(verbose=1)
+
+        self._episodes_log_interval = episodes_log_interval
+        self._episode_counter = 0
+
+    def per_step(self):
+        pass
+
+    def per_episode(self):
+        pass
+
+    def per_episode_interval(self):
+        pass
+
+    def _on_step(self) -> bool:
+        self.per_step()
+
+        if self.locals["dones"]:
+            self.per_episode()
+            self._episode_counter += 1
+
+        if self._episode_counter == self._episodes_log_interval:
+            self.per_episode_interval()
+            self._episode_counter = 0
+
+        return True
---
+++ rl/utils/callbacks/logger.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import wandb
+
+from rl.utils.callbacks.base import Callback, Episode
+
+
+class BaseLogCallback(Callback):
+
+    def __init__(self, episodes_log_interval):
+        super().__init__(episodes_log_interval)
+
+        self.data = []
+        self._curr_episode = Episode()
+
+    def per_step(self):
+        infos = self.locals["infos"][0]
+        infos["sb3_iteration"] = self.locals["iteration"]
+        infos["sb3_n_steps"] = self.locals["n_steps"]
+        infos["sb3_reward"] = self.locals["rewards"]
+        infos["sb3_action"] = self.locals["actions"]
+        infos["sb3_done"] = self.locals["dones"]
+
+        self._curr_episode.data.append(infos)
+
+    def per_episode(self):
+        self.data.append(self._curr_episode)
+        self._curr_episode = Episode()
+
+    def per_episode_interval(self):
+        self.base_log()
+        self.data = []
+
+    def _base_metrics(self, full_df, episode_end_df):
+        return dict(mean_sb3_reward=full_df["sb3_reward"].mean(),
+                    mean_episode_len=sum(len(ep) for ep in self.data) / len(self.data))
+
+    def base_log(self):
+        full_df = pd.concat([ep.to_df() for ep in self.data])
+        episode_end_df = pd.concat([ep.to_df().tail(1) for ep in self.data])
+
+        base_metrics = self._base_metrics(full_df, episode_end_df)
+        additional_metrics = self.metrics(full_df, episode_end_df)
+
+        wandb.log(base_metrics | additional_metrics)
+
+    def metrics(self, full_df, episode_end_df):
+        return {}
