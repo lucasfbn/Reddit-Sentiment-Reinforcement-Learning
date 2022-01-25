91799d0ee2c47c98f5e9a596b2d063f4431696a2
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-25 17:32:37 +0000

    Update stock_dataset.py

--- dataset_handler/stock_dataset.py
+++ dataset_handler/stock_dataset.py
@@ -1,12 +1,18 @@
-from dataset_handler.meta.parser import Parser as MetaParser
-from dataset_handler.data.parser import Parser as DataParser
-from dataset_handler.meta.saver import Saver as MetaSaver
-from dataset_handler.data.saver import Saver as DataSaver
-from pathlib import Path
 import os
+import tempfile
+from pathlib import Path
+
+import wandb
+
+from dataset_handler.data.parser import Parser as DataParser
+from dataset_handler.data.saver import Saver as DataSaver
+from dataset_handler.meta.parser import Parser as MetaParser
+from dataset_handler.meta.saver import Saver as MetaSaver
 
 
 class StockDataset:
+    META_FN = "meta.json"
+    DATA_FN = "data.h5"
 
     def __init__(self, root):
         self.root = Path(root)
@@ -18,72 +24,91 @@ class StockDataset:
 
         self._data = None
 
-    def dump(self, data, dump_data=True):
-        meta_saver = MetaSaver(self.root / "meta.h5")
-        meta_saver.dump(data)
+    @property
+    def data(self):
+        return self._data
+
+    def dump(self, dump_data=True):
+        meta_saver = MetaSaver(self.root / self.META_FN)
+        meta_saver.dump(self._data)
 
         if dump_data:
-            data_saver = DataSaver(self.root / "data.h5")
-            data_saver.dump_multiple(data)
+            data_saver = DataSaver(self.root / self.DATA_FN)
+            data_saver.dump_multiple(self._data)
             data_saver.close()
 
     def load_meta(self):
         if self._meta_parser is None:
-            self._meta_parser = MetaParser(self.root / "meta.h5")
+            self._meta_parser = MetaParser(self.root / self.META_FN)
 
         self._data = self._meta_parser.parse()
 
     def _init_parser(self):
         if self._data_parser is None:
-            self._data_parser = DataParser(self.root / "data.h5")
+            self._data_parser = DataParser(self.root / self.DATA_FN)
 
-    def load_data_single(self, idx):
-        self._init_parser()
-        self._data[idx] = self._data_parser.parse_single(self._data[idx])
-        return self._data[idx]
+    def _close_parser(self):
+        if self._data_parser is not None:
+            self._data_parser.close()
 
     def load_data(self):
         self._init_parser()
         self._data = self._data_parser.parse_multiple(self._data)
-        self.close_parser()
-        return self._data
+        self._close_parser()
 
-    def close_parser(self):
-        if self._data_parser is not None:
-            self._data_parser.close()
+    def __len__(self):
+        return len(self._data)
+
+    def __getitem__(self, idx):
+        return self._data[idx]
+
+    def __iter__(self):
+        yield from self._data
 
 
 class StockDatasetWandb(StockDataset):
 
-    def download(self, run, fn, version, type):
+    def __init__(self, run, version):
+        super().__init__(self._get_root(run, fn="dataset", version=version, type="dataset"))
+
+    def _get_root(self, run, fn, version, type):
         art = run.use_artifact(f'{run.entity}/{run.project}/{fn}:v{version}', type=type)
-        self.root = Path(art.download(root=os.getenv("WANDB_DIR") + "artefacts/dataset"))
+        return Path(art.download(root=os.getenv("WANDB_DIR") + "artefacts/dataset"))
 
-    def log(self):
-        art = wandb.Artifact("dataset", type="dataset")
-        art.add_dir(self.root)
-        wandb.log_artifact(art)
+    def wandb_load(self, load_data=False):
+        self.load_meta()
+        if load_data:
+            self.load_data()
+
+    def log_as_artifact(self, log_data=False):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            self.root = Path(tmpdirname)
+            self.dump(dump_data=log_data)
+
+            art = wandb.Artifact("dataset", type="dataset")
+            art.add_file((self.root / self.META_FN).as_posix())
+
+            if log_data:
+                art.add_file((self.root / self.DATA_FN).as_posix())
+
+            wandb.log_artifact(art)
+
+    def log_as_file(self, run, log_data=False):
+        self.root = Path(run.dir)
+        self.dump(log_data)
 
 
-import pickle as pkl
-import wandb
+if __name__ == "__main__":
+    def non_wandb_usage():
+        dataset = StockDataset(r"F:\wandb\artefacts\dataset")
+        dataset.load_meta()
+        dataset.load_data()
+        dataset.dump(dump_data=True)
 
-# path = r"F:\wandb\artefacts\dataset.pkl"
-# # path = r"temp3.pkl"
-#
-# with open(path, "rb") as f:
-#     data = pkl.load(f)
-import time
 
-dataset = StockDataset(r"F:\wandb\artefacts\dataset")
-
-start = time.time()
-# dataset.dump(data, dump_data=True)
-dataset.load_meta()
-print(time.time() - start)
-dataset.load_data()
-print(time.time() - start)
-
-time.sleep(100)
-
-print()
+    def wandb_usage():
+        with wandb.init(project="TestsProject", group="Throwaway") as run:
+            dataset = StockDatasetWandb(run, 0)
+            dataset.wandb_load()
+            # # dataset.log_as_file(run, log_data=True)
+            # dataset.log_as_artifact(log_data=True)
