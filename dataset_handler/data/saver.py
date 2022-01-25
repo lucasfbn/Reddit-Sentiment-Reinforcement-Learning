58ebd6874c2d08d883e175d10fa57de6177c9762
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-25 17:32:28 +0000

    Minor fixes

--- dataset_handler/data/saver.py
+++ dataset_handler/data/saver.py
@@ -13,14 +13,10 @@ class Saver:
     def dump_single(self, obj):
         name = obj.name
 
-        obj.df["date_day_shifted"] = obj.df["date_day_shifted"].astype(str)
-        obj.df["date_day_shifted"] = pd.to_datetime(obj.df["date_day_shifted"], format="%Y-%m-%d")
-        obj.df["date_day_shifted"] = (obj.df["date_day_shifted"] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
-
         self.store.create_dataset(f"{name}/COLS", data=np.array(list(obj.df.columns), dtype="S"))
         self.store.create_dataset(f"{name}/DF", data=obj.df.to_numpy(dtype="float32"))
 
-        sequences = tuple(seq.data.arr.to_numpy() for seq in obj.sequences.lst)
+        sequences = tuple(seq.data.arr for seq in obj.sequences.lst)
         seq_stacked = np.stack(sequences, axis=0)
 
         self.store.create_dataset(f"{name}/SEQ", data=seq_stacked)
