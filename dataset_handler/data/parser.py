00e3f73a6f13ba45107846d92d811268cce0f0ba
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-25 16:26:17 +0000

    data/meta parser/saver

---
+++ dataset_handler/data/parser.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import h5py
+
+
+class Parser:
+
+    def __init__(self, fp):
+        self.fp = fp
+        self.store = h5py.File(self.fp, "r")
+
+    def parse_single(self, obj):
+        name = obj.name
+
+        cols = self.store[f"{name}/COLS"][:].astype(str)
+        arr = self.store[f"{name}/DF"][:]
+
+        obj.df = pd.DataFrame(data=arr, columns=cols)
+
+        sequences = self.store[f"{name}/SEQ"]
+
+        for i, seq in enumerate(obj.sequences):
+            seq.data.arr = sequences[i]
+        return obj
+
+    def parse_multiple(self, data):
+        for d in data:
+            self.parse_single(d)
+        return data
+
+    def close(self):
+        self.store.close()
