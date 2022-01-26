52363301bed519c2f7ad210cc5989dd5a3545bca
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-26 11:51:08 +0000

    Added step

--- rl/utils/callbacks/logger.py
+++ rl/utils/callbacks/logger.py
@@ -56,7 +56,7 @@ class BaseLogCallback(Callback):
         base_metrics = self._base_metrics(full_df, episode_end_df)
         additional_metrics = self.metrics(full_df, episode_end_df)
 
-        wandb.log(base_metrics | additional_metrics)
+        wandb.log(base_metrics | additional_metrics, step=self.num_timesteps)
 
     def metrics(self, full_df, episode_end_df):
         return {}
