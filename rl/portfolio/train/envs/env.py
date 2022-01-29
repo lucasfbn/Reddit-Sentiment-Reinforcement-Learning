69350e956389c245e2417bc474d989306293caad
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-29 21:57:54 +0000

    trial 10.2

--- rl/portfolio/train/envs/env.py
+++ rl/portfolio/train/envs/env.py
@@ -68,7 +68,7 @@ class BaseEnv(Env, ABC):
         reward_handler = RewardHandler()
         # reward = reward_handler.discount_cash_bound(reward, seq.evl.days_cash_bound)
 
-        penalty_base = -1.0
+        penalty_base = -5.0
         inv_ratio, _ = reward_handler.inv_trades_ratio(inv_len=self.trading_env.inventory.inv_len(),
                                                        n_trades=self.trading_env.n_trades)
         factor = reward_handler.penalize_ratio(inv_ratio)
