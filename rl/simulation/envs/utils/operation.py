d96eeea6117da8773255ba2eeb205c17d012b6ab
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-31 12:29:20 +0000

    Deleted obsolete files

--- rl/simulation/envs/utils/operation.py
+++
@@ -1,8 +0,0 @@
-class Operation:
-
-    def __init__(self, ticker, sequence):
-        self.tradeable = sequence.metadata.tradeable
-        self.date = sequence.metadata.date
-        self.price = sequence.metadata.price_raw
-        self.ticker = ticker.name
-        self.sequence = sequence
