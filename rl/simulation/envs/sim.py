91e37fd1c8ff5a3f444445c7fff265903679b29a
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-31 13:06:55 +0000

    Further callbacks

--- rl/simulation/envs/sim.py
+++ rl/simulation/envs/sim.py
@@ -48,7 +48,10 @@ class Simulation:
     def per_action_callback(self, day, success, sequence):
         pass
 
-    def end_of_day_callback(self, day):
+    def start_of_day_callback(self, day):
+        pass
+
+    def end_of_day_callback(self, day, actions):
         pass
 
     def end_of_eval_callback(self):
@@ -56,7 +59,9 @@ class Simulation:
 
     def eval_loop(self):
 
-        for day, sequences in tqdm(self._daywise_sequences.items(), desc="Processing day"):
+        for i, (day, sequences) in tqdm(enumerate(self._daywise_sequences.items()), desc="Processing day"):
+
+            self.start_of_day_callback(i)
 
             actions = {"0": [], "1": [], "2": []}
 
@@ -83,7 +88,7 @@ class Simulation:
                 success = self._trading_env.buy(buy)
                 self.per_action_callback(day, success, buy)
 
-            self.end_of_day_callback(day)
+            self.end_of_day_callback(i, actions)
             self._trading_env.reset_day()
 
         self.end_of_eval_callback()
@@ -91,10 +96,19 @@ class Simulation:
 
 class SimulationWandb(Simulation):
 
-    def end_of_day_callback(self, day):
-        wandb.log(dict(day=str(day),
+    def start_of_day_callback(self, day):
+        wandb.log(dict(day=day,
                        inventory_len=len(self._trading_env.inventory),
                        balance=self._trading_env.balance))
+
+    def end_of_day_callback(self, day, actions):
+        holds, buys, sells = actions["0"], actions["1"], actions["2"]
+        total = len(holds) + len(buys) + len(sells)
+
+        wandb.log(dict(day=day,
+                       buy_ratio=len(buys) / total,
+                       hold_ratio=len(holds) / total,
+                       sell_ratio=len(sells) / total))
 
     def end_of_eval_callback(self):
         log_to_summary(wandb.run, dict(final_balance=self._trading_env.balance,
