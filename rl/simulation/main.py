c34dcd10f8e489bf3dbd82e797cf59789589f022
Lucas Fobian <50679363+lucasfbn@users.noreply.github.com>
2022-01-31 17:06:20 +0000

    Now uses config

--- rl/simulation/main.py
+++ rl/simulation/main.py
@@ -4,18 +4,18 @@ from dataset_handler.stock_dataset import StockDatasetWandb
 from rl.simulation.envs.sim import SimulationWandb
 
 
-def main(meta_file_run_id):
-    with wandb.init(project="Trendstuff", group="Simulation") as run:
+def main(config):
+    with wandb.init(project="Trendstuff", group="Simulation", config=config) as run:
         dataset = StockDatasetWandb()
-        dataset.wandb_load_meta_file(meta_file_run_id, run)
+        dataset.wandb_load_meta_file(config["meta_file_run_id"], run)
 
         dataset = dataset
         eval_env = SimulationWandb(dataset)
         eval_env.prepare_data()
-        eval_env.eval_loop()
+        eval_env.eval_loop(config["skip_first_day"])
 
         wandb.config.update(dict(
-            meta_file_run_id=meta_file_run_id,
+            meta_file_run_id=config["meta_file_run_id"],
             start_balance=eval_env.trading_env.START_BALANCE,
             investment_per_trade=eval_env.trading_env.INVESTMENT_PER_TRADE,
             max_price_per_stock=eval_env.trading_env.MAX_PRICE_PER_STOCK,
@@ -24,4 +24,9 @@ def main(meta_file_run_id):
         ))
 
 
-main("as6t2wi1")
+config = {
+    "meta_file_run_id": "as6t2wi1",
+    "skip_first_day": True
+}
+
+main(config)
